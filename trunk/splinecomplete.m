e99cc7710a725a41b4d434ef7a071e9ade198ea9
Tetsuro Kurita <tkurita@mac.com>
2007-06-01 12:15:31 +0000

    [Octave-f] splinecomplete.m octave 2.9 では、trisolve が消えている。spdiagsでスパース行列を作り連立方程式を解く方式に変更
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@29 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/splinecomplete.m
+++ trunk/splinecomplete.m
@@ -44,51 +44,58 @@
 ## Paul Kienzle, 19. feb 2001,  csape supports now matrix y value
 
 function pp = splinecomplete (x, y, valc)
-
+  
   x = x(:);
   n = length(x);
-  if n < 3, error("csape requires at least 3 points"); endif
-
+  if n < 3
+    error("splinecomplete requires at least 3 points")
+  endif
+  
   transpose = (columns(y) == n);
-  if (transpose) y = y'; endif
-
+  if (transpose)
+    y = y';
+  endif
+  
   a = y;
   b = c = zeros (size (y));
   h = diff (x);
   idx = ones (columns(y),1);
-
-
-	if (nargin < 3)
-		valc = [0, 0];
-	endif
-	
-	dg(2:n-1) = 2 * (h(1:n-2) .+ h(2:n-1));
-	dg(1) = 2*h(1);
-	dg(n) = 2*h(n-1);
-	dg=dg';
-	
-	delta = diff(a)./h;
-	g(2:n-1) = delta(2:n-1) .- delta(1:n-2);
-	g(1) = delta(1) - valc(1);
-	g(n) = valc(2) - delta(n-1);
-	g=g';
-	
-	sigma = trisolve(dg,h,g);
-	
-	b=delta - h.*(sigma(2:n) + 2.*sigma(1:n-1));
-	c = (3 * sigma(1:n-1));
-	d = (sigma(2:n) - sigma(1:n-1)) ./ h;
-	a = a(1:n-1);
-	
-	coeffs = [d(:), c(:), b(:), a(:)];
-	pp = mkpp (x, coeffs);
-
+  
+  
+  if (nargin < 3)
+    valc = [0, 0];
+  endif
+  
+  dg(2:n-1) = 2 * (h(1:n-2) .+ h(2:n-1));
+  dg(1) = 2*h(1);
+  dg(n) = 2*h(n-1);
+  dg=dg';
+  
+  delta = diff(a)./h;
+  g(2:n-1) = delta(2:n-1) .- delta(1:n-2);
+  g(1) = delta(1) - valc(1);
+  g(n) = valc(2) - delta(n-1);
+  g=g';
+  
+  #sigma = trisolve(dg,h,g);
+  #return
+  dg_len = length(dg);
+  sigma = spdiags([[h(:);0],dg(:),[0;h(:)]], [-1,0,1], dg_len, dg_len)\g(:);
+  
+  b = delta - h.*(sigma(2:n) + 2.*sigma(1:n-1));
+  c = (3 * sigma(1:n-1));
+  d = (sigma(2:n) - sigma(1:n-1)) ./ h;
+  a = a(1:n-1);
+  
+  coeffs = [d(:), c(:), b(:), a(:)];
+  pp = mkpp (x, coeffs);
+  
 endfunction
 
 function p = slope(delta,h,sigma,x,targetx)
-	w = (targetx - x)./h;
-	invw = 1 -w;
-	p = delta + h ((3*w^2-1).* sigma(2:n-1) + (3*invw^2-1).* sigma(1:n-2));
+  w = (targetx - x)./h;
+  invw = 1 -w;
+  p = delta + h ((3*w^2-1).* sigma(2:n-1) + (3*invw^2-1).* sigma(1:n-2));
 endfunction
 
 %!shared x,y,cond
