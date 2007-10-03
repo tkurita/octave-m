19b4d1cd6eb03ba0c5582427a00793a288fe2e29
Tetsuro Kurita <tkurita@mac.com>
2007-10-03 05:41:13 +0000

    [Octave-f] totalMatForTrack.m some modification and add comments
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@59 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/Tracking/totalMatForTrack.m
+++ trunk/Tracking/totalMatForTrack.m
@@ -1,3 +1,8 @@
+##== History
+## 2007-10-03
+## * should be obsolute, because dispersion is not supported.
+## * new function is mat_for_tracking
+
 function result = totalMatForTrack(latticeArray)
   result = {};
   for i = 1:length(latticeArray)
@@ -5,11 +10,12 @@ function result = totalMatForTrack(latticeArray)
     totMat.v = eye(3);
     pCells = latticeArray{i};
     for j = 1:length(pCells)
-      cell = pCells{j};
-      totMat.h = cell.mat.h * totMat.h;
-      totMat.v = cell.mat.v * totMat.v;
+      a_cell = pCells{j};
+      totMat.h = a_cell.mat.h * totMat.h;
+      totMat.v = a_cell.mat.v * totMat.v;
     endfor
     totMat = [totMat.h(1:2,1:2), zeros(2); zeros(2), totMat.v(1:2,1:2) ];
+    #totMat = [totMat.h, zeros(3); zeros(3), totMat.v ];
     result = {result{:},totMat};
   endfor
 endfunction
