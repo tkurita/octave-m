2fabb9ac4a1718cedfacc060a491bd0f10223318
Tetsuro Kurita <tkurita@mac.com>
2007-10-03 09:00:23 +0000

    [Octave-f] steererKick.m : add comments. will be obsolete
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@63 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/Tracking/steererKick.m
+++ trunk/Tracking/steererKick.m
@@ -1,3 +1,8 @@
+##== History
+## 2007-10-03
+## * should be obsoluted
+## * use kicker_kick
+
 function result = steererKick(steerer, particles)
   particles = steerer.halfDuct * particles;
   result = steerer.halfDuct * (particles + repmat(steerer.kickVector, 1, size(particles)(2)));
