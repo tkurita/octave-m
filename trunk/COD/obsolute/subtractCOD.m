7af1cabe5a067249860e30ec0842e624c1c69060
Tetsuro Kurita <tkurita@mac.com>
2007-10-03 00:36:19 +0000

    [Octave-f] subtractCOD is obsolute
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@52 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/COD/obsolute/subtractCOD.m
+++ trunk/COD/obsolute/subtractCOD.m
@@ -1,10 +1,16 @@
-## usage : result = subtractCOD(cod1,cod2)
-## 
+## Usage : result = subtractCOD(cod1,cod2)
+##  oboslute. use subtract_cod
+##  
 ## COD の 差分を計算する。
 ## cod1 - cod2
 ##
 ##= Parameters
 ## cod.BPM_name = positon [mm]
+
+##== History
+## * 2007.10.02
+## obsolute
+
 
 function result = subtractCOD(cod1,cod2)
   
