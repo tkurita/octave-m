87c0c88c37918b146f24b248f0cdf3fd01bd1bb2
Tetsuro Kurita <tkurita@mac.com>
2007-10-03 12:00:28 +0000

    [Octave-f] mat_for_tracking.m
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@64 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/Tracking/mat_for_tracking.m
+++ trunk/Tracking/mat_for_tracking.m
@@ -1,26 +1,27 @@
-## Usage : result = mat_for_tracking(elements)
+## Usage : mat_rec = mat_for_tracking(elements)
 ##
 ##== Parameters
 ## * elements : a cell array of elements of a lattice
-
+##              or a single element structure
+## 
 ##== History
 ## 2007-10-03
+## * don't accept cell array of cell array
 ## * derived from totalMatForTrack
 ## * add support dispersion 
 
-function result = mat_for_tracking(elements)
-  result = {};
-  for i = 1:length(elements)
-    totMat.h = eye(3);
-    totMat.v = eye(3);
-    pCells = elements{i};
-    for j = 1:length(pCells)
-      a_cell = pCells{j};
-      totMat.h = a_cell.mat.h * totMat.h;
-      totMat.v = a_cell.mat.v * totMat.v;
+function mat_rec = mat_for_tracking(elements)
+  if isstruct(elements)
+    a_mat = mat_with_elememnt(elements)
+  else
+    a_mat = eye(6);
+    for n = 1:length(elements)
+      a_mat = a_mat * mat_with_elememnt(elements{n})
     endfor
-    totMat = [totMat.h, zeros(3); zeros(3), totMat.v ];
-    result = {result{:},totMat};
-  endfor
-endfunction
+  endif
   
+  mat_rec.mat = a_mat;
+  mat_rec.apply = @through_mat;
+  mat_rec.name = "matrix span";
+endfunction
+
