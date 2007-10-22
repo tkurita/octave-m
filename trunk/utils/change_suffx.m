e8f5385e499ee4dc696fdde15693d1fbd31a5016
Tetsuro Kurita <tkurita@mac.com>
2007-10-22 05:25:18 +0000

    [Octave-f] change_suffx.m
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@88 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/utils/change_suffx.m
+++ trunk/utils/change_suffx.m
@@ -1,3 +1,16 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{new_filename} =} change_suffix(@var{filename}, @var{new_suffix})
+##
+## change the suffix/extension of @var{filename}
+##
+## @example
+## exchangeSuffix("/path1/name1.txt", ".dat")
+## > ans = /path1/name1.dat
+## @end example
+##
+## @end deftypefn
+
+
 ## Usage : result = exchangeSuffix(filename)
 ##          filename の拡張子を変更する
 ##
@@ -5,8 +18,8 @@
 ## exchangeSuffix("/path1/name1.txt", ".dat")
 ## > ans = /path1/name1.dat
 
-function result = exchangeSuffix(filename, newSuffix)
-  result = [stripSuffix(filename), newSuffix];
+function result = change_suffix(filename, new_suffix)
+  result = [strip_suffix(filename), new_suffix];
 endfunction
   
   
