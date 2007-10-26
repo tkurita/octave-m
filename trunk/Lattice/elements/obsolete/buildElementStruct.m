efb1692b53847d93bbb62070db35817aea88d859
Tetsuro Kurita <tkurita@mac.com>
2007-10-26 00:48:35 +0000

    [Octave-f] buildElememntStruct is obsolete
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@102 a4934b6f-310c-0410-bed4-ac220e363503

---
+++ trunk/Lattice/elements/obsolete/buildElementStruct.m
@@ -0,0 +1,22 @@
+## -- usage : inStr = buildElementStruct(inStr, matFunc, horv)
+
+function inStr = buildElementStruct(inStr, matFunc, horv)
+  [len, hasEfflen] = fieldLength(inStr);
+  ## full
+  inStr.mat.(horv) = matFunc(inStr);
+  
+  if (hasEfflen)
+    dl = (inStr.efflen - inStr.len)/2;
+    dlMat = DTmat(-dl);
+    inStr.mat.(horv) =  dlMat * inStr.mat.(horv) * dlMat;
+  endif
+  
+  inStr.twmat.(horv) = twpMatrix(inStr.mat.(horv));
+  
+  ## half
+  inStr.mat_half.(horv) = matFunc(setfield(inStr, "efflen", len/2));
+  if (hasEfflen)
+    inStr.mat_half.(horv) = dlMat * inStr.mat_half.(horv);
+  endif
+  inStr.twmat_half.(horv) = twpMatrix(inStr.mat_half.(horv));
+endfunction
