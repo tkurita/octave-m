fe2e1458b95a423500ec9ddac5f14d5af76575bf
Tetsuro Kurita <tkurita@mac.com>
2007-11-26 01:30:40 +0000

    [Octave-f] ESD accept one argument of a structure
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@146 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/BeamOpticsElements/ElectrostaticSectorField/ESD.m
+++ trunk/m/BeamOpticsElements/ElectrostaticSectorField/ESD.m
@@ -1,13 +1,34 @@
-function esd_struct = ESD(prop, a_name, varargin)
-  len = prop.radius*prop.angle;
-  esd_struct = setfields(prop, "len", len, "name", a_name, "kind", "ESD");  
-  
-  esd_struct.mat.h = ESCylindrical_mat(esd_struct);
-  esd_struct.mat.v = DTmat(len);
-  
-  if (length(varargin) > 0)
-    if (ismatrix(varargin))
-      esd_struct.duct = varargin{n};
+## @deftypefn {Function File} {} func_name(@var{property_struct})
+## @deftypefnx {Function File} {} func_name(@var{prop}, @var{name} [,@var{aparture}])
+##
+## Make a electrostatic deflector object
+##
+## @end deftypefn
+
+##== History
+## 2007-11-26
+## * accepet one argument of a structure
+
+#function esd_struct = ESD(prop, a_name, varargin)
+function esd_struct = ESD(varargin)
+  if (length(varargin) == 1)
+    if (isstruct(varargin{1}))
+      esd_struct = varargin{1};
+    else
+      error("Invalid argument.");
     endif
+  else
+    esd_struct = setfields(varargin{1}, "name", varargin{2});  
+    if (length(varargin) > 2)
+      if (ismatrix(varargin{3}))
+        esd_struct.duct = varargin{3};
+      endif
+    endif
+    
   endif
+  
+  esd_struct.len = esd_struct.radius*esd_struct.angle;  
+  esd_struct.mat.h = ESCylindrical_mat(esd_struct);
+  esd_struct.mat.v = DTmat(esd_struct.len);
+  esd_struct.kind = "ESD";
 endfunction
