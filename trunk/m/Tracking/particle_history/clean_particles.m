6495af2dc840a554fb856fe0a9b877af335cbf44
Tetsuro Kurita <tkurita@mac.com>
2008-01-16 14:33:06 +0000

    [Octave-f] particle_history/cleanParticles.m is obsolete. partilce_hisoty/clean_particles.m is introduced.
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@179 a4934b6f-310c-0410-bed4-ac220e363503

---
+++ trunk/m/Tracking/particle_history/clean_particles.m
@@ -0,0 +1,22 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {} clean_particles(@var{inparticles})
+##
+## Return a matrix removing rows which have NaN or large values at (1,:) from @var{inparticeles}
+##
+## Every rows of @{invarticles} indicate points of each particles in the phase sepace.
+##
+##
+## @end deftypefn
+
+##== History
+## 2008-01-16
+## * derived from cleanParticles
+
+function outparticles = clean_particles(inparticles)
+  #size(inparticles)
+  particles = inparticles(:,!isnan(inparticles(1,:))); #NaN の除去  
+  outparticles = particles(:,abs(particles(1,:)) < 0.5); #大きな振幅の粒子を除去
+  if (!length(outparticles))
+    error("No valid particles.\n");
+  endif
+endfunction
--- trunk/m/Tracking/particle_history/obsolete/cleanParticles.m
+++ trunk/m/Tracking/particle_history/obsolete/cleanParticles.m
@@ -1,5 +1,10 @@
+##== History
+## 2008-01-16
+## * obsolete. use clean_particles.m
+
 function outparticles = cleanParticles(inparticles)
   #size(inparticles)
+  warning("cleanParticles is obsolete. Use clean_particles");
   particles = inparticles(:,!isnan(inparticles(1,:))); #NaN の除去  
   outparticles = particles(:,abs(particles(1,:)) < 0.5); #大きな振幅の粒子を除去
   if (!length(outparticles))
