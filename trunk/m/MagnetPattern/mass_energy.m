d91bb2b07852a46a1786316745aa4608f1462fb0
Tetsuro Kurita <tkurita@mac.com>
2008-02-26 04:17:08 +0000

    [Octave-f] mass_energy.m can accept mass number.
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@188 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/MagnetPattern/mass_energy.m
+++ trunk/m/MagnetPattern/mass_energy.m
@@ -5,17 +5,26 @@
 ## * particle -- "proton" or "carbon" or "helium" or mass number [amu]
 
 ##== History
-## 
-function result = mass_energy(particle)
-  switch particle
-    case "proton"
-      result = physical_constant("PROTON_MASS_ENERGY_EQUIVALENT_IN_MEV");
-    case "helium"
-       result = physical_constant("ALPHA_PARTICLE_MASS_ENERGY_EQUIVALENT_IN_MEV");
-    case "carbon"
-      amu = physical_constant("ATOMIC_MASS_CONSTANT_ENERGY_EQUIVALENT_IN_MEV");
-      result = amu *12;
-    otherwise
+## 2008-02-26
+## * Use physical_constant intead of physicalConstant.
+## * mass_energy can accelpt mass number
 
-  endswitch
+function result = mass_energy(particle)
+  if (isnumeric(particle))
+    amu = physical_constant("ATOMIC_MASS_CONSTANT_ENERGY_EQUIVALENT_IN_MEV");
+    result = amu * particle;
+  else
+    switch particle
+      case "proton"
+        result = physical_constant("PROTON_MASS_ENERGY_EQUIVALENT_IN_MEV");
+      case "helium"
+         result = physical_constant("ALPHA_PARTICLE_MASS_ENERGY_EQUIVALENT_IN_MEV");
+      case "carbon"
+        amu = physical_constant("ATOMIC_MASS_CONSTANT_ENERGY_EQUIVALENT_IN_MEV");
+        result = amu *12;
+      otherwise
+        error("\"%s\" is unknown particle specifier.", particle);
+    endswitch
+  endif
+   
 endfunction
