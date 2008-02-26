3f562cac3b38bfa7fd671533746761b2054b703b
Tetsuro Kurita <tkurita@mac.com>
2008-02-26 01:35:32 +0000

    [Octaive-f] physicalConstant.m is obsolete. use Octave-forge\'s physical_consnt
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@184 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/utils/FourierTransform/fourier.m
+++ trunk/m/utils/FourierTransform/fourier.m
@@ -1,22 +1,32 @@
-## usage : fftRec = fourier(fftRec)
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{fft_rec} =} fourier(@var{fft_rec})
+## 
+## The input @var{fft_rec} is a structure which has following fields
+## @table @code
+## @item data
+## one dimendional matrix
+## @item interval
+## time interval between data points [sec]
+## @end table
 ##
-## = Paramerters
-## * fftRec must have following data
-##  .data
-##  .interval : time interval between data points [sec]
-##
-## * result
-##  .fftResult
-##  .amplitude
-##  .frequency
+## The following field added into @var{fft_rec} as output.
+## @table @code
+## @item fft_result
+## output of fft function
+## @item amplitude
+## abs(fft_result)
+## @item frequency
+## frequency according to the amplitude
+## @end table
+## @end deftypefn
 
-function fftRec = fourier(fftRec)
-  fftResult = fft(fftRec.data);
-  nsample = length(fftRec.data);
-  amplitude = abs(fft(fftRec.data));
-  halfN = floor(nsample/2);
-  amplitude = amplitude(1:halfN);
-  delf = 1/(fftRec.interval * nsample);
-  frequency = 0:delf:((halfN-1)*delf);
-  fftRec = append_fields(fftRec, fftResult, amplitude, frequency);
+function fft_rec = fourier(fft_rec)
+  fft_result = fft(fft_rec.data);
+  nsample = length(fft_rec.data);
+  amplitude = abs(fft(fft_rec.data));
+  n_half = floor(nsample/2);
+  amplitude = amplitude(1:n_half);
+  delf = 1/(fft_rec.interval * nsample);
+  frequency = 0:delf:((n_half-1)*delf);
+  fft_rec = append_fields(fft_rec, fft_result, amplitude, frequency);
 endfunction
