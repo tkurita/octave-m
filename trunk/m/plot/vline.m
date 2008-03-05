072fcca564d67cbfb68150106e15dd61aab01b0c
Tetsuro Kurita <tkurita@mac.com>
2008-03-05 05:07:33 +0000

    [Octave-f] vline.m can accept vector
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@192 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/plot/vline.m
+++ trunk/m/plot/vline.m
@@ -13,6 +13,20 @@
 
 function result = vline(x, varargin)
   ca = gca();
-  ylim = get(ca, "ylim");
-  result = line([x,x], [ylim(1), ylim(2)], varargin{:});  
+  global __ylim;
+  __ylim = get(ca, "ylim");
+  global __prop;
+  __prop = varargin;
+  result = map(@_vline, x);
+endfunction
+
+function result = _vline(x)
+  global __ylim;
+  global __prop;
+  
+  if (length(__prop) > 0)
+    result = line([x,x], ylim, __prop{:});  
+  else
+    result = line([x,x], ylim);  
+  endif
 endfunction
