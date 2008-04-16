9a554ad8b14b17d22e2a2b13d234941de2adac1f
Tetsuro Kurita <tkurita@mac.com>
2008-04-16 16:00:45 +0000

    [Octave-f] duct_aperture.m : Returned value has "shape" field. If the shape is "circle", the returnd value has "radius" field.
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@215 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/BeamOpticsElements/duct_aperture.m
+++ trunk/m/BeamOpticsElements/duct_aperture.m
@@ -12,22 +12,34 @@
 ## @item xmin
 ## @item ymax
 ## @item ymin
+## @item radius
+## If the number of arguments is only one, this field is set.
+##
+## @item shape
+## If the number of argument is only one, this field is "circle", otherwise "rect". 
 ## @end table
 ##
 ## @end deftypefn
 
+##== History
+## 2008-04-17
+## * returned value has "shape" field. 
+## * If the shape is "circle", the returnd value has "radius" field.
 
-function aperture_struct = duct_aperture(a_size)
+function retval = duct_aperture(a_size)
   switch length(a_size)
     case 1
-      aperture_struct = struct("xmax", a_size(1), "xmin", -a_size(1)... 
-                      , "ymax", a_size(1), "ymin", -a_size(1));
+      retval = struct("xmax", a_size(1), "xmin", -a_size(1)... 
+                              , "ymax", a_size(1), "ymin", -a_size(1)...
+                              , "radius", a_size(1), "shape", "circle");
     case 2
-      aperture_struct = struct("xmax", a_size(1), "xmin", -a_size(1)...
-                      , "ymax", a_size(2), "ymin", -a_size(2));
+      retval = struct("xmax", a_size(1), "xmin", -a_size(1)...
+                      , "ymax", a_size(2), "ymin", -a_size(2)...
+                      , "shape", "rect");
     case 4
-      aperture_struct = struct("xmax", a_size(1), "xmin", a_size(2)...
-                      , "ymax", a_size(3), "ymin", a_size(4));  
+      retval = struct("xmax", a_size(1), "xmin", a_size(2)...
+                             , "ymax", a_size(3), "ymin", a_size(4)...
+                             , "shape", "rect");  
     otherwise
       a_size
       error("The matrix of duct size is invalid.");
