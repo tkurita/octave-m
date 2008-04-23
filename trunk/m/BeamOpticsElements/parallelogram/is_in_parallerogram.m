9bb0cfe5548f629fd619e9a06fce01a2ba882cff
Tetsuro Kurita <tkurita@mac.com>
2008-04-23 13:23:45 +0000

    [Octave-f] in_in_pallerogram.m : chenge implementaion using inpolygon. may accept xy_point as a matrix (multiple points).
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@222 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/BeamOpticsElements/parallelogram/is_in_parallerogram.m
+++ trunk/m/BeamOpticsElements/parallelogram/is_in_parallerogram.m
@@ -7,11 +7,15 @@
 
 
 ##== History
+## 2008-04-23
+## * chenge implementaion using inpolygon.
+## * may accept xy_point as a matrix (multiple points).
+## 
 ## 2008-03-13
 ## * first implementaion
 
 #shareTerm /Users/tkurita/WorkSpace/シンクロトロン/2008.01 Tracking/extraction/extraction.m
-function result = is_in_parallerogram(a_para, xy_point, horv)
+function retval = is_in_parallerogram(a_para, xy_point, horv)
   switch horv
     case "h"
       xory = "x";
@@ -19,9 +23,16 @@ function result = is_in_parallerogram(a_para, xy_point, horv)
       xory = "y";
   endswitch
   
+  xv = [a_para.([xory, "min"]), a_para.([xory, "max"]), a_para.([xory, "max"]), a_para.([xory, "min"])];
+  xdv = [a_para.([xory, "dash_min"]).l, a_para.([xory, "dash_max"]).l, a_para.([xory, "dash_max"]).h, a_para.([xory, "dash_min"]).h];
+
+  retval = inpolygon(xy_point(:,1), xy_point(:,2), xv, xdv); 
+end
+
+function retval = process1(a_para, xory)
   if(! ((xy_point(1) > a_para.([xory,"min"])) & (xy_point(1) < a_para.([xory,"max"]))))
     #"not in xrange"
-    result = false;
+    retval = false;
     return;
   end
   
@@ -31,7 +42,7 @@ function result = is_in_parallerogram(a_para, xy_point, horv)
   
   if (xy_point(2) <= xl)
     #"xdash is too small"
-    result = false;
+    retval = false;
     return;
   end
   
@@ -41,9 +52,9 @@ function result = is_in_parallerogram(a_para, xy_point, horv)
   
   if (xy_point(2) >= xh)
     #"xdash is too large"
-    result = false;
+    retval = false;
     return;
   end
   
-  result = true;
+  retval = true;
 end
