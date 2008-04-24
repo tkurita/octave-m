f2b542a7a4a35ce84a897fda9ecbf312cbf37e6a
Tetsuro Kurita <tkurita@mac.com>
2008-04-24 03:54:25 +0000

    [Octave-f] calcKickerValue.m : wrote fuction help with texinfo format.
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@223 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/COD/calcKickerValue.m
+++ trunk/m/COD/calcKickerValue.m
@@ -1,9 +1,25 @@
-## Usage : iroB = calcKickerValue(targetElement, kickAngle, brho)
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{iorB} =} calcKickerValue(@var{element}, @var{kick_angle}, @var{brho})
 ##
-## = Parameters
-## * targetElement : the element to calculate kick angle
-## * iorB : current of magnet or magnetic filed. depend on targetElement
-## * brho
+## Convert kick angle at the @var{element} into the current [A] or magnetic field [T].
+##
+## Arguments
+## @table @code
+## @item @var{element}
+## A kicker object to calculate current or magnetic field.
+## 
+## @item @var{kick_angle}
+## kick angle at @var{element}. The unit is radian.
+##
+## @item @var{brho}
+## The b*rho value of the particle. The unit is [T*m].
+## @end table
+##
+## @end deftypefn
+
+##== History
+## 2008-04-24
+## * wrote function help with texinfo.
 
 function iorB = calcKickerValue(targetElement, kickAngle, brho)
   iorB = convertKickValues(targetElement, kickAngle, brho, false);
