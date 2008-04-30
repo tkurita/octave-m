e1480680714bd66a1c8ba4ca62b2fdbb5e01951f
Tetsuro Kurita <tkurita@mac.com>
2008-04-30 11:21:57 +0000

    [Octave-f] momentumCompactionFactor.m : Use is_BM intead of isBendingMagnet
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@230 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/Lattice/momentumCompactionFactor.m
+++ trunk/m/Lattice/momentumCompactionFactor.m
@@ -6,13 +6,16 @@
 ##* allElements: すべての要素の matrix の配列。
 ##              calcLattice によって分散（field eater）が計算されている必要がある。
 
+##== History
+## * Use is_BM intead of isBendingMagnet
+
 function alpha = momentumCompactionFactor(allElements)
   ##calc mometum compaction factor
   alpha = 0;
   for n = 1:length(allElements)
     theElement = allElements{n};
     
-    if (isBendingMagnet(theElement))
+    if (is_BM(theElement))
       theEater = (allElements{n-1}.eater(1)+theElement.eater(1))/2;
       alpha += theEater*theElement.bmangle;
     endif
