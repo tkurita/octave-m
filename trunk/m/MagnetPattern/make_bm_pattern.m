8aa97db79832ff6b8f76464f8053eafa0a33187e
Tetsuro Kurita <tkurita@mac.com>
2008-08-06 02:14:30 +0000

    [Octave-f] add make_bm_pattern and smooth_trapz
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@250 a4934b6f-310c-0410-bed4-ac220e363503

---
+++ trunk/m/MagnetPattern/make_bm_pattern.m
@@ -0,0 +1,72 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {} make_bm_pattern(@var{bmin}, @var{bmax}, @var{bgrad}, @var{tstep}, @var{smoothstep})
+##
+## @end deftypefn
+
+##== History
+## 2008-08-05
+## * first implementation
+
+function varargout = make_bm_pattern(bmin, bmax, bgrad, tstart, smoothstep)
+  trange1 = tstart:smoothstep:(tstart+smoothstep*2);
+  b1 = cubic_sloop_base(trange1, [bmin, 0,0], [bgrad, 0]);
+  
+  trange2 = 0:smoothstep:(0+smoothstep*2);
+  b2 = cubic_sloop_top(trange2, [bgrad, 0], [bmax, 0, 0]);
+  trange2 = trange2 + trange1(end) + (b2(1) - b1(end))/bgrad;
+  retval = [[trange1';trange2'], [b1'; b2']];
+  if (nargout < 1)
+    for n = 1:rows(retval)
+      printf("%5.1f\t%7.5f\n", retval(n,1), retval(n,2));
+    endfor
+  else
+    varargout = {retval};
+  endif
+endfunction
+
+function retval = cubic_sloop_base(t, ys, ye)
+  [x1, x2, x3] = div_elem(t);
+  [y1, y1d, y1dd] = div_elem(ys);
+  [y3d, y3dd] = div_elem(ye);
+  Y = [y1; y1d; y1dd; 0; 0; 0; y3d; y3dd];
+  X = [0, 0, 0, 1,  0, 0, 0, 0; # y1
+       0, 0, 1, 0,  0, 0, 0, 0; # y1d
+       0, 2, 0, 0,  0, 0, 0, 0; # y1dd
+       (x2-x1)^3,   (x2-x1)^2, (x2-x1), 1,  0, 0, 0,  -1; # y2
+       3*(x2-x1)^2, 2*(x2-x1), 1,       0,  0, 0, -1, 0;  # y2d
+       6*(x2-x1),   2,         0,       0,  0, -2, 0, 0;  # y2dd
+       0,           0,         0,       0,  3*(x3-x2)^2,  2*(x3-x2),  1, 0; # y3d
+       0,           0,         0,       0,  6*(x3-x2),    2,          0, 0]; # y3dd
+  A = X\Y;
+  pp = mkpp(t, [A(1:4)';A(5:8)'] );
+  retval = ppval(pp, t);
+endfunction
+
+function retval = cubic_sloop_top(t, ys, ye)
+  [x1, x2, x3] = div_elem(t);
+  [y1d, y1dd] = div_elem(ys);
+  [y3, y3d, y3dd] = div_elem(ye);
+  Y = [y1d; y1dd; 0; 0; 0; y3; y3d; y3dd];
+#  X = [3*x1^2, 2*x1, 1,  0,  0,0,0,0;
+#       6*x1,   2,    0,  0,  0,0,0,0;
+#       x2^3,   x2^2, x2, 1,  -x2^3,   -x2^2, -x2, -1;
+#       3*x2^2, 2*x2, 1,  0,  -3*x2^2, -2*x2, -1,   0;
+#       6*x2,  2,     0,  0,  -6*x2,   -2*x2,  0,   0;
+#       0,0,0,0,  x3^3,   x3^2, x3, 1;
+#       0,0,0,0,  3*x3^2, 2*x3, 1,  0;
+#       0,0,0,0,  6*x3,   2,    0,  0]
+  X = [0,0,1,0,  0,0,0,0;
+       0,2,0,0,  0,0,0,0;
+       (x2-x1)^3,   (x2-x1)^2, (x2-x1), 1,  0,0, 0,-1;
+       3*(x2-x1)^2, 2*(x2-x1), 1,       0,  0,0,-1, 0;
+       6*(x2-x1),   2,         0,       0,  0,-2, 0, 0;
+       0,0,0,0,  (x3-x2)^3,   (x3-x2)^2, (x3-x2), 1;
+       0,0,0,0,  3*(x3-x2)^2, 2*(x3-x2), 1,  0;
+       0,0,0,0,  6*(x3-x2),   2,    0,  0];
+  A = X\Y;
+  retval = ppval(mkpp(t, [A(1:4)'; A(5:8)']), t);
+endfunction
+
+%!test
+%! bmin=0.3662; bmax=1.7067; bgrad=0.002375924; tstart=35; smoothstep=25;
+%! make_bm_pattern(bmin, bmax, bgrad, tstart, smoothstep)
