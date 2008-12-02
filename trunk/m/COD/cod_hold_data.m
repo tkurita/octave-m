c0d8783c6c186b248b0bb0e00b6020d079291600
Tetsuro Kurita <tkurita@mac.com>
2008-12-02 09:31:23 +0000

    [Octave-f] support new format which BPM arraaned along with real machine.
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@269 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/COD/cod_hold_data.m
+++ trunk/m/COD/cod_hold_data.m
@@ -1,19 +1,23 @@
 ## usage : 
+##
+
+##== Histtory
+## 2008-11-28
+## * support new format which BPM arraaned along with real machine.
+
 function cod_at_BPM = cod_hold_data(filename, FBorFT)
   csv_data = csvread(filename);
   switch (FBorFT)
     case ("FT")
-      a_row = csv_data(3,:);
+      id = csv_data(2, 2:end);
+      a_row = csv_data(3,2:end);
     case ("FB")
-      a_row = csv_data(5,:);
+      id = csv_data(4, 2:end);
+      a_row = csv_data(5,2:end);
   endswitch
   
-  cod_at_BPM.BPM1 = a_row(2);
-  cod_at_BPM.BPM2 = a_row(3);
-  cod_at_BPM.BPM3 = a_row(4);
-  cod_at_BPM.BPM4 = a_row(5);
-  cod_at_BPM.BPM5 = a_row(6);
-  cod_at_BPM.BPM6 = a_row(7);
-  cod_at_BPM.BPM7 = a_row(8);
+  for n = 1:length(id)
+    cod_at_BPM.(sprintf("BPM%d", id(n))) = a_row(n);
+  endfor
   
 endfunction
