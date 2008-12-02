765854fc911fe4556ab941256ceab91a1ba96ea7
Tetsuro Kurita <tkurita@mac.com>
2008-12-02 15:11:18 +0000

    [Octave-f] bvalues_for_period is renamed from BValuesForTimes
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@273 a4934b6f-310c-0410-bed4-ac220e363503

---
+++ trunk/m/MagnetPattern/bvalues_for_period.m
@@ -0,0 +1,79 @@
+## usage [bLine, tLine] = bvalues_for_period(patternSet, tStep, startTime, endTime)
+##
+##= Parameters
+## tStep : time interval [msec]
+## startTime and endTime can be ommited.
+##
+##= Result
+## bLine : BL value [T*m]
+
+##== History
+## 2008-12-03
+## * renamed from BValuesForTimes
+
+function [bLine, tLine] = bvalues_for_period(patternSet, varargin)
+  # patternSet = BMPattern
+  # timeInfo = {1,35,85}
+  timeInfo = varargin;
+  tStep = timeInfo{1};
+  
+  tPoints = patternSet{1}.tPoints;
+  tLine = timeLine(tPoints,tStep);
+  bLine = interpRegion(patternSet{1},tLine);
+  
+  nRegion = length(patternSet);
+  for n = 2:nRegion
+    #patternSet{i}
+    # n = 2;
+    # 
+    tPoints = patternSet{n}.tPoints;
+    tLineTmp = timeLine(tPoints,tStep);
+    bLineTmp = interpRegion(patternSet{n},tLineTmp);
+    tLine = [tLine,tLineTmp];
+    bLine = [bLine,bLineTmp];
+  endfor
+  
+  if (length(timeInfo) > 1)
+    startTime = timeInfo{2};
+    if (tLine(1) > startTime)
+      headdingTime = startTime:tStep:(tLine(1)-tStep);
+      tLine = [headdingTime,tLine];
+      headdingB(1:length(headdingTime)) = bLine(1);
+      bLine = [headdingB, bLine];
+    elseif (tLine(1) < startTime)
+      n = 1;
+      while(tLine(n) < startTime)
+        n++;
+      endwhile
+      tLine = tLine(n:length(tLine));
+      bLine = bLine(n:length(bLine));
+    endif
+  endif
+  
+  if (length(timeInfo) > 2)
+    endTime = timeInfo{3};
+    if (tLine(end) < endTime)
+      enddingTime = (tLine(end)+tStep):tStep:endTime;
+      tLine = [tLine,enddingTime];
+      enddingB(1:length(enddingTime)) = bLine(end);
+      bLine = [bLine,enddingB];
+    elseif (tLine(end) > endTime)
+      n = length(tLine)-1;
+      while(tLine(n) > endTime)
+        n--;
+      endwhile
+      tLine = tLine(1:n);
+      bLine = bLine(1:n);
+    endif
+    
+  endif
+endfunction
+
+function tLine = timeLine(tPoints,tStep)
+  if (tStep < 1)
+    tLine = (tPoints(1)+tStep):tStep:tPoints(end);
+  else
+    tLine = roundup(tPoints(1)):tStep:rounddown(tPoints(end));
+  endif
+  
+endfunction
