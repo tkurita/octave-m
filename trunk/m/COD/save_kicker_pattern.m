32c11e823e7a90ae697f876e75a67af37297b303
Tetsuro Kurita <tkurita@mac.com>
2008-12-03 13:37:55 +0000

    [Octave-f] add save_kicker_pattern.m
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@278 a4934b6f-310c-0410-bed4-ac220e363503

---
+++ trunk/m/COD/save_kicker_pattern.m
@@ -0,0 +1,49 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {} save_kicker_pattern(@var{file}, @var{t}, @var{cod_rec_FB}, @var{cod_rec_FT})
+## Output pattern file of kickers from COD Record objects
+## 
+## @table @code
+## @item @var{t}
+## Timmings for trapezoidal pattern. The length must be 14;
+## @item @var{cod_rec_FB}
+## COD Record Object for Flat Base
+## @item @var{cod_rec_FT}
+## COD Record Object for Flat Top
+## @end table
+## 
+## @end deftypefn
+
+##== History
+## 2008-12-03
+## * first implementation
+
+function retval = save_kicker_pattern(file, t, cod_rec_FB, cod_rec_FT)
+  # file = "testout"
+  kickers = cod_rec_FB.steererNames;
+  val_fb = cod_rec_FB.steererValues;
+  val_ft = cod_rec_FT.steererValues;
+  [fid, msg] = fopen(file, "w");
+  for n = 1:length(val_fb)
+    fprintf(fid, "#\n%s,\n", convname(kickers{n}));
+    bpat = trapz_pattern(t, val_fb(n), val_ft(n));
+    csv_with_bpattern(bpat, fid);
+  endfor
+  fclose(fid);
+endfunction
+
+function retval = convname(name)
+  name_table = {"St1I", "STH1";
+                "St2I", "STH2";
+                "St3I", "STH3";
+                "St4I", "STH4";
+                "St5I", "STH5";
+                "St6I", "STH6"};
+  for n = 1:rows(name_table)
+    name_row = name_table(n,:);
+    if (contain_str(name_row, name))
+      retval = name_row{1};
+    endif
+  endfor
+endfunction
+%!test
+%! save_kicker_pattern(x)
