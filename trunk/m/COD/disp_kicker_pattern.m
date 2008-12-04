d8f24aa4ed0a123b26a1bf139ac4c7d9265d2c0e
Tetsuro Kurita <tkurita@mac.com>
2008-12-04 08:31:42 +0000

    [Octave-f] add disp_kicker_pattern
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@283 a4934b6f-310c-0410-bed4-ac220e363503

---
+++ trunk/m/COD/disp_kicker_pattern.m
@@ -0,0 +1,58 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {} disp_kicker_pattern(@var{t}, @var{cod_rec_FB}, @var{cod_rec_FT}, [@var{fid}])
+## Output pattern file of kickers from COD Record objects with table form.
+## 
+## Steerer values are inverted.
+## 
+## @table @code
+## @item @var{t}
+## Timmings for trapezoidal pattern. The length must be 14;
+## @item @var{cod_rec_FB}
+## COD Record Object for Flat Base
+## @item @var{cod_rec_FT}
+## COD Record Object for Flat Top
+## @item @var{fid}
+## Screaming number. The default is 1 i.e. stdout.
+## @end table
+## 
+## @end deftypefn
+
+##== History
+## 2008-12-03
+## * first implementation
+
+function retval = disp_kicker_pattern(t, cod_rec_FB, cod_rec_FT, varargout)
+  if (length(varargout))
+    fid = varargout{1};
+  else
+    fid = stdout;
+  endif
+  # fid = popen("pbcopy", "w")
+  # file = "testout"
+  kickers = cod_rec_FB.steererNames;
+  val_fb = cod_rec_FB.steererValues;
+  val_ft = cod_rec_FT.steererValues;
+  bpat_cells = {}
+  for n = 1:length(val_fb)
+    bpat = trapz_pattern(t, val_fb(n), val_ft(n));
+    bpat_cells{end+1} = bpat;
+  endfor
+  
+  bpat_mat = mat_with_bpattern(bpat_cells{1});
+  for n = 2:length(bpat_cells)
+    a_mat = mat_with_bpattern(bpat_cells{n});
+    bpat_mat(:,n+1) = a_mat(:,2);
+  endfor
+  
+  fprintf(fid, "time\t%s\n", joincell2string(kickers, "\t"));
+  template = ftemplate_for_mat(bpat_mat(1,2:end), "precision", "%.2f", "delim", "\t");
+  template = ["%.1f\t", template];
+  for n = 1:rows(bpat_mat)
+    fprintf(fid, template, bpat_mat(n, :));
+  endfor
+  
+endfunction
+
+
+%!test
+%! disp_kicker_pattern(x)
