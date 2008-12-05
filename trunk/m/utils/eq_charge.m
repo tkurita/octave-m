dde1945fa53ec8f96c974b293e75e0b3989564a9
Tetsuro Kurita <tkurita@mac.com>
2008-12-05 12:30:35 +0000

    [Octave-f] add eq_charge
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@295 a4934b6f-310c-0410-bed4-ac220e363503

---
+++ trunk/m/utils/eq_charge.m
@@ -0,0 +1,36 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {[@var{q}, @var{dq}] =} eq_charge(@var{z}, @var{particle}, @var{mev})
+## 
+## Return equilibrium charge state and distribution width
+## 
+## @table @code
+## @item @var{z}
+## Atomic number
+## @item @var{particle}
+## Mass number
+## @item @var{mev}
+## Kinetic Energy in MeV
+## @end table
+##
+## @table @code
+## @item @var{q}
+## equilibrium charge state
+## @item @var{dq}
+## width of charge state distribution
+## @end table
+## @end deftypefn
+
+##== History
+##
+
+function [q, dq] = eq_charge(z, particle, mev)
+  # mev = 5
+  # z = 29
+  # particle = 64
+  b = beta_with_mev(mev, particle);
+  q = z*(1-1.08*exp(-80.1*(z^-0.506)*(b^0.996)));
+  dq = 0.32*z^0.45;
+endfunction
+
+%!test
+%! eq_charge(x)
