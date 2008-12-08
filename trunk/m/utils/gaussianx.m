5a8eda55a0a4d617e159783af52e284f94e95be8
Tetsuro Kurita <tkurita@mac.com>
2008-12-08 14:11:07 +0000

    [Octave-f] gaussianx: add normalized amplitude mode
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@298 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/utils/gaussianx.m
+++ trunk/m/utils/gaussianx.m
@@ -2,9 +2,24 @@
 ## @deftypefn {Function File} {} gaussianx(@var{x}, @var{amp}, @var{sigma}, @var{mu})
 ## @deftypefnx {Function File} {} gaussianx(@var{x}, [@var{amp}, @var{sigma}, @var{mu}])
 ##
+## Return value of gaussian distribution as @var{x}.
+## 
+## @var{amp}*exp(-1*(@var{x} - @var{mu}).^2/(2*@var{sigma}^2));
+##
+## @table @code
+## @item @var{amp}
+## Amplitude. When 0 is passed, normalized amplitude (1/(@var{sigma}*sqrt(2*pi)) is used.
+## @item @var{sigma}
+## standard deviation
+## @item @var{mu}
+## mean
+## @end table
+## 
 ## @end deftypefn
 
 ##== History
+## 2008-12-08
+## * Add normalized amplitude mode.
 ## 2008-07-02
 ## * rename to gaussianx from gaussian, bcause there is gaussian function in signal package.
 ##
@@ -15,7 +30,9 @@ function result = gaussianx(x, amp, sigma, mu)
     mu = amp(3);
     amp = amp(1);
   endif
-  
+  if (!amp)
+    amp = (1/(sigma*sqrt(2*pi)));
+  endif
   result = amp*exp(-1*(x - mu).^2/(2*sigma^2));
 endfunction
 
