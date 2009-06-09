86a50fb4da705a22c3208c8899624673ceefaad2
Tetsuro Kurita <tkurita@mac.com>
2009-06-09 18:03:00 +0000

    [Octave-f] isInRegion.m * t can accept vector * return element-by-element result of t
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@321 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/MagnetPattern/isInRegion.m
+++ trunk/m/MagnetPattern/isInRegion.m
@@ -1,3 +1,9 @@
+
+##== History
+## 2009-06-10
+## * t can accept vector
+## * return element-by-element result of t
+
 function bool = isInRegion(theList, t)
-  bool = ((theList(1) <= t)&&(t <= last(theList)));
+  bool = ((theList(1) <= t) & (t <= theList(end)));
 endfunction
