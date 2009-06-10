16fe7e6563c903674bc890b675962ba50bc72d0e
Tetsuro Kurita <tkurita@mac.com>
2009-06-10 01:18:26 +0000

    [Octave-f] BValuesAtTimes is obsolete
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@325 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/MagnetPattern/obsolete/BValuesAtTimes.m
+++ trunk/m/MagnetPattern/obsolete/BValuesAtTimes.m
@@ -1,6 +1,7 @@
 ## usage : bLine = BValuesAtTimes(BPattern, tLine)
 
 function bLine = BValuesAtTimes(BPattern, tLine)
+  warning("BValuesAtTimes is obsolete. use value_at_time.");
 	bLine = [];
 	for i = 1:length(tLine)
 		bLine = [bLine; BValueAtTime(BPattern, tLine(i))];
--- trunk/m/MagnetPattern/value_at_time.m
+++ trunk/m/MagnetPattern/value_at_time.m
@@ -5,22 +5,24 @@
 
 ##== History
 ## 2008-07-28
+## * a vector is accepted as t
+## * improve algorithm
 ## * renamed from BValueAtTime
 
 function b = value_at_time(bpattern, t)
-  if (!nargin < 2)
+  if (nargin < 2)
     print_usage();
   endif
   # bpattern = BMPattern
   # t = [1,2,3]
+  b = NA(size(t));
   for n = 1:length(bpattern)
     # n = 1 
     r = bpattern{n};
     flags = is_in_range(r.tPoints, t);
     ind = find(flags);
-    b = interpRegion(t(ind))
-    if (is_in_range(r.tPoints, t))
-      b = interpRegion(r,t);
+    if length(ind)
+      b(ind) = interp_in_region(r, t(ind));
     endif
   endfor
 endfunction
