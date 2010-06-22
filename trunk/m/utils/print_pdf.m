49aa653cfea6d84b491b439e764d4bbad6622ace
Tetsuro Kurita <tkurita@mac.com>
2010-06-22 05:19:36 +0000

    [Octave-f] print_pdf.m
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@345 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/utils/print_pdf.m
+++ trunk/m/utils/print_pdf.m
@@ -2,13 +2,23 @@
 ## @deftypefn {Function File} {@var{result} =} print_pdf(@var{fname})
 ##
 ## Output plot as PDF file of which font size is 10 and size is 8,5.
+##
 ## @end deftypefn
 
 ##== History
 ##
 
 function retval = print_pdf(fname)
-  print("-S8,5","-F:10","-dpdf", fname);
+  #xy = [11, 8.5];
+  xy = [8, 5];
+  orient("landscape");
+  set(gcf, "papersize", xy);
+  set(gcf, "paperposition", [0,0,xy(1),xy(2)]);
+  #set(gcf, "paperposition", [0,0,8,5]);
+  #print("-S8,5","-F:10","-dpdf", fname);
+  print("-F:10", "-dpdf", fname); # -F は legend だけに効く
+  #print("-dpdf", fname); 
+  #print("-S12,10","-F:14","-dpdf", fname);
 endfunction
 
 %!test
