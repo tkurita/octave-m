b4edcf2f3d44e5c523d873220450f0f8cb5053a3
Tetsuro Kurita <tkurita@mac.com>
2010-07-29 13:15:29 +0000

    [Octave-f] factorY.m : add comment
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@351 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/xydata/obsolete/factorY.m
+++ trunk/m/xydata/obsolete/factorY.m
@@ -1,5 +1,6 @@
 ## Usage : result = factorY(xydata, yfactor)
 ##      result = [xydata(:,1), xydata(:,2)*yfactor];
+## Deprecated. Use calc_xy
 
 function result = factorY(xydata, yfactor)
   result = [xydata(:,1), xydata(:,2)*yfactor];
