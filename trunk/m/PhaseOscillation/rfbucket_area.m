439e808f116fc2cfa734e796a3a4e8c8f45978fd
Tetsuro Kurita <tkurita@mac.com>
2010-12-21 02:10:21 +0000

    [Octave-f] renamed RFBucketEArea to rfbucket_area
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@365 a4934b6f-310c-0410-bed4-ac220e363503

---
+++ trunk/m/PhaseOscillation/rfbucket_area.m
@@ -0,0 +1,32 @@
+## usage : [eArea,eArea2,deltaEmax,phi1] = RFbucketEArea(
+##                                vList, nM, Ee, eta, h, phi_s, C)
+## parameters : 
+## vList : RF Voltage Pattern, must be (voltage * charge state)
+## phi_s : phase angle of synchronus particle
+## Ee : Total energy
+## nM : mass number of the particle
+## C : シンクロトロンの周長
+## 
+## results : 
+## eArea : RF Bucket Area in deltaE-phi coordinate [eV]
+## eArea2 : RF Bucket Area in deltaE/(h omega_rev) -phi coordinate [ev sec]
+## deltaEmax : bucket height [eV]
+## phi1 : unstable fixed point
+
+function [eArea,eArea2,deltaEmax,phi1] = RFbucketEArea(vList, nM, Ee, eta, h, phi_s,C)
+  global proton_eV;
+  global lv;
+
+  for n = 1:length(phi_s)
+	fs = phi_s(n);
+	[alphaArea(n),yHeight(n), phi1(n)] = calcBucketSize(fs);
+  endfor
+
+  m0c2 = nM*proton_eV;
+  beta2 = 1 - (m0c2^2)./(Ee.^2);
+  eArea = 16.*sqrt(vList .* beta2 .*Ee./(2*pi*h.*abs(eta))).*alphaArea;
+##  omega_rev = 2*pi.*sqrt(beta2).*lv./C;
+##  eArea2 = eArea./(h .*omega_rev); 
+  eArea2 = 16.*(C/lv) .* sqrt(vList .* Ee./((2*pi*h)^3.*abs(eta))).*alphaArea;
+  deltaEmax = sqrt(vList .* beta2./h) .* yHeight .*sqrt(abs(Ee./(pi.*eta)));
+endfunction
