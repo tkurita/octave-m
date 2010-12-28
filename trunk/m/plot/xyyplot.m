e9157f2e2a3bcb5bbc1fc1e858e6a92feb5bed62
Tetsuro Kurita <tkurita@mac.com>
2010-12-28 05:12:14 +0000

    [Octave-f] xyplot.m : First implementation for Octave 3.2
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@379 a4934b6f-310c-0410-bed4-ac220e363503

---
+++ trunk/m/plot/xyyplot.m
@@ -0,0 +1,83 @@
+
+## usage : xyyplot({[X,Y], format ...}, {[X, Y2], format ...})
+##
+## accept column wise 2 dimensional matrix as X-Y data
+
+##== History
+## 2010-12-28
+## * First implementation for Octave 3.2
+
+function ax = xyyplot(xy1arg, xy2arg)
+  f = get(0, "currentfigure");
+  if (isempty(f))
+    f = figure();
+  endif
+  
+  ca = get(f, "currentaxes");
+  is_plotyy_axes = false;
+  if (isempty(ca))
+    ax = [];
+  elseif (strcmp(get(ca, "tag"), "plotyy"));
+    ax = get(ca, "__plotyy_axes__");
+    is_plotyy_axes = true;
+  else
+    ax = ca;
+  endif
+  
+  if (length(ax) > 2)
+    for n = 3:length(ax)
+      delete(ax(n));
+    endfor
+    ax = ax(1:2);
+  elseif (length(ax) == 1)
+    ax(2) = axes();
+  elseif (isempty(ax))
+    ax(1) = axes();
+    ax(2) = axes();
+  endif
+  
+  axes(ax(1))
+  newplot()
+  xyplot(xy1arg{:});
+  set(ax(1), "keypos", "northwest");
+  cf = gcf();
+  set(cf, "nextplot", "add");
+  
+  axes(ax(2));
+  newplot();
+  xyplot(xy2arg{:});
+  
+  set(ax(2), "yaxislocation", "right");
+  set(ax(2), "position", get(ax(1), "position"));
+  xmax = realmin;
+  xmin = realmax;
+  for n = 1:length(ax)
+    lines = get(ax(n), "children");
+    for m = 1:length(lines)
+      if (strcmp("line", get(lines(m), "type")))
+        xdata =get(lines(m), "xdata");
+        xmax = max([xdata(:); xmax]);
+        xmin = min([xdata(:); xmin]);
+      endif
+    endfor
+  endfor
+  xl = [xmin, xmax];
+  set(ax(1), "xlim", xl);
+  set(ax(2), "xlim", xl);
+  set(ax(2), "xticklabelmode", "manual");
+  
+  ## Tag the plotyy axes, so we can use that information
+  ## not to mirror the y axis tick marks
+  set(ax, "tag", "plotyy")
+
+  ## Store the axes handles for the sister axes.
+  if (is_plotyy_axes) 
+    set(ax(1), "__plotyy_axes__", ax);
+    set(ax(2), "__plotyy_axes__", ax);
+  else
+    addproperty ("__plotyy_axes__", ax(1), "data", ax);
+    addproperty ("__plotyy_axes__", ax(2), "data", ax);
+  endif
+
+  set(cf, "nextplot", "replace");
+endfunction
