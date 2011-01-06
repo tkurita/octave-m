a8c5b96d44b1402d396514c5af70bdaca825425e
Tetsuro Kurita <tkurita@mac.com>
2011-01-06 16:34:17 +0000

    [Octave-f] plotPhaseOsillation2.m : reimplemeted for Octave 3.2
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@386 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/PhaseOscillation/plotPhaseOsillation2.m
+++ trunk/m/PhaseOscillation/plotPhaseOsillation2.m
@@ -3,53 +3,31 @@
 ## bucket area in dR/(h omega)-phi coordinate
 ## pattern of bending magnet and RF voltage
 
-function plotPhaseOsillation2(tLine,bLine,bPoints_set,vList,omega_s,phi_s,eArea2)
+##==History
+## 2011-01-07
+## * reimplemented for Octave 3.2
 
-  global lv; # light velocity
-  global proton_ev; 
-
-  bLine_set = [tLine;bLine]';
-  vLine_set = [tLine;vList]';
-  phi_s_set = [tLine;phi_s .*360./(2*pi)]';
-  eArea2_set = [tLine; eArea2]'; #[msec; 10 eV sec]
-  omega_s_set = [tLine;omega_s./1000]';
-
-  automatic_replot=0;
-  gset nomultiplot
-  ##common seetting
-  gset size 0.8,0.5
-  gset rmargin 10
-  gset lmargin 10
-  gset y2tics
-  gset ytics nomirror
-
-  gset multiplot
-  ##fist plot: BM Pattern and RF Voltage pattern
-  gset origin 0,0
-  gset bmargin
-  gset xtics
-  gset format x "%g"
-  gset xlabel "Time [msec]"
-  gset ylabel 1,0
-  gset ylabel "BL value of BM [T*m]"
-  gset y2label "RF Voltage [V]"
-  gset y2range [0:450]
-  gplot bLine_set title "BM Pattern",\
-	  bPoints_set with points title "",\
-	  vLine_set axes x1y2 title "RF Voltage"
-
-  ## common settings for second plot
-  gset noxlabel
-  gset bmargin 0
-  gset y2range [*:*]
-  gset format x ""
+function plotPhaseOsillation2(tline,bline,bPoints_set,vlist,omega_s,phi_s,e_area2)
+  # bPoints_set = bp;
+  # phi_s = ps;
+  #   
+  bline_set = [tline,bline];
+  vline_set = [tline,vlist];
+  phi_s_set = [tline, phi_s .*360./(2*pi)];
+  e_area2_set = [tline, e_area2] ; #[msec; 10 eV sec]
+  omega_s_set = [tline, omega_s./1000];
   
-  ## second plot
-  gset origin 0,0.5
-  gset ylabel "synchrotorn Frequency [KHz]\\nphase angle [degree]"
-  gset y2label "RF bucket area [eV*sec]"
-  gplot omega_s_set title "synchrotorn Frequency [KHz]", \
-	  phi_s_set title "phase angle of synchronus particle [degree]",\
-	  eArea2_set axes x1y2 title "bucket Area"
+  subplot(2,1,1);
+  ax = xyyplot({omega_s_set, "-;synchrotorn Frequency [KHz];",...
+                phi_s_set, "-;phase angle of synchronus particle [degree];"},...
+                {e_area2_set, "-r;bucket Area;"});...
+  ylabel(ax(1), "synchrotorn Frequency [KHz]\nphase angle [degree]");...
+  ylabel(ax(2), "RF bucket area [eV*sec]");
+  subplot(2,1,2);
+  ax = xyyplot({bline_set, "-;BM Pattern;", bPoints_set, "*"},...
+               {vline_set, "-g;RF Voltage;"});...
+  ylabel(ax(1), "BL value of BM [T*m]");...
+  ylabel(ax(2), "RF Voltage [V]");...
+  ylim(ax(2), [0,450]);xlabel("Time [msec]");
 
 endfunction
