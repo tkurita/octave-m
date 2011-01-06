5eda03fe8a7dfac70ce9b1fbbbd90fe7505043ef
Tetsuro Kurita <tkurita@mac.com>
2011-01-06 13:38:16 +0000

    [Octave-f] plotPhaseOsillation.m:reimplemented for Octave 3.2
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@384 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/PhaseOscillation/plotPhaseOsillation.m
+++ trunk/m/PhaseOscillation/plotPhaseOsillation.m
@@ -7,6 +7,9 @@
 ## vlist: RF Voltage Pattern
 ## bline: pattern of bending magnet
 
+##= History
+## * reimplemented for Octave 3.2
+
 function plotPhaseOsillation(tline,bline,bPoints_set,vlist,omega_s,phi_s,deltaEFromB,e_area, d_emax,nM,totalEnergy,phi1)
   # phi_s = ps;
   # deltaEFromB = d_e;
@@ -29,73 +32,28 @@ function plotPhaseOsillation(tline,bline,bPoints_set,vlist,omega_s,phi_s,deltaEF
 
   relative_e_area_set = [tline, (e_area./(kEList*4*pi)).*100]; #[msec; %]
 
-  subplot(3,1,3);
-  xyyplot({bline_set,"-;BM Pattern;", bPoints_set, "*"},...
-          {vLine_set, "-;RF Voltage;"});
-  subplot(3,1,2);
-  xyplot(d_emax_set, "-;RF bucket height [KeV];",...
-        e_area_set, "-;Bucket Area [10 KeV];")
   subplot(3,1,1);
-  xyplot(omega_s_set, "-;synchrotorn Frequency [KHz];",...
-        phi_s_set, "-;phase angle of synchronus particle [degree];");
+  ax = xyyplot({omega_s_set, "-;synchrotorn Frequency [KHz];"},...
+               {phi_s_set, "-g;phase angle of synchronus particle [degree];"});...
+  ylabel(ax(1), "synchrotorn Frequency [kHz]");...
+  ylabel(ax(2), "phase angle [degree]");
+  #tickslabel_off(ax(1), "x");  
+  subplot(3,1,2);
+  ax = xyyplot(
+      {d_emax_set, "-;RF bucket height [KeV];",...
+        e_area_set, "-;Bucket Area [10 KeV];",...
+        deltaEFromB_set, "-r;Energy gain/1 BClocl [KeV];"},...
+      {relative_e_area_set, "-c;Relative Bucket Area [%];"});...
+  ylabel(ax(1), "Bucket Height [KeV]\nBucket Area [10KeV]");...
+  ylabel(ax(2), "Relative Bucket Area [%]");
+  #tickslabel_off("x");
+  subplot(3,1,3);
+  ax = xyyplot({bline_set,"-;BM Pattern;", bPoints_set, "*"},...
+              {vLine_set, "-g;RF Voltage;"}, "linecolor", "green");...
+  ylabel(ax(1), "BL value of BM [T m]");...
+  ylabel(ax(2), "RF Voltage [V]");...
+  ylim(ax(2), [0,500]);xlabel("Time [msec]");
 
+  #print_pdf("phase_oscillation.pdf", "fontsize", 10, "papersize", [8, 10])
 
-  automatic_replot=0;
-  gset nomultiplot
-  #oneplot()
-
-  ##common seetting
-  gset size 0.8,0.33
-  gset rmargin 10
-  gset lmargin 10
-  gset y2tics
-  gset ytics nomirror
-  
-  
-  gset multiplot
-  #multiplot(1,3)
-  ##fist plot: BM Pattern and RF Voltage pattern
-  gset origin 0,0
-  gset bmargin
-  gset xtics
-  gset format x "%g"
-  xlabel("Time [msec]")
-  gset ylabel 1,0
-  ylabel("BL value of BM [T m]")
-  y2label("RF Voltage [V]")
-  y2range(0,700)
-
-  gplot bline_set title "BM Pattern",\
- 	  bPoints_set with points title "",\
- 	  vLine_set axes x1y2 title "RF Voltage"
-
-  ## common settings for second plot
-  gset noxlabel
-  xlabel("")
-  gset bmargin 0
-  gset noxlabel
-  gset format x ""
-  gset y2range [*:*]
-  y2range();
-
-  ## second plot: RF bucket height and energy gain
-  gset origin 0,0.33
-  gset ylabel "Bucket Height [KeV]\\nBucket Area [10KeV]"
-  #ylabel("Bucket Height [KeV]\\nBucket Area [10KeV]");
-#  y2label("Relative Bucket Area [%]")
-  y2label("")
-  
-  gplot d_emax_set title "RF bucket height [KeV]",\
-	  e_area_set title "Bucket Area [10 KeV]" #,\
-								#deltaEFromB_set title "Energy gain/1 BClocl [KeV]",\
-	  	  #relative_e_area_set axes x1y2 title "Relative Bucket Area [%]"
-
-  ## thrid plot
-  gset origin 0,0.66
-  gset ylabel "synchrotorn Frequency [KHz]"
-  gset y2label "phase angle [degree]"
-  gplot omega_s_set title "synchrotorn Frequency [KHz]", \
-	  phi_s_set axes x1y2 title "phase angle of synchronus particle [degree]"
-
-  automatic_replot=1;
 endfunction
