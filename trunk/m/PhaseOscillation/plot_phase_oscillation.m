147da7fa3f8b5984bf95168598b1598b590ff031
Tetsuro Kurita <tkurita@mac.com>
2011-01-25 13:22:36 +0000

    [Octave-f] fixed conversion from [rad/sec] to [kHz]
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@400 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/PhaseOscillation/plot_phase_oscillation.m
+++ trunk/m/PhaseOscillation/plot_phase_oscillation.m
@@ -1,17 +1,32 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {} plot_phase_oscillation(@var{tline}, @var{bline}, @var{bpoints}, @var{vlist}, @var{ws}, @var{deltaEFromB}, @var{e_area}, @var{d_emac}, @var{nM}, @var{totalEnergy}, @var{phi1})
 ## plot ...
-## * synchrotron frequency and Phase angle of synchronus particle
-## * bucket height, bucket Area in dE-phi coordinate
-## * pattern of bending magnet and RF voltage
+## @itemize
+## @item synchrotron frequency and Phase angle of synchronus particle.
+## @item bucket height, bucket Area in dE-phi coordinate.
+## @item pattern of bending magnet and RF voltage.
+## @end itemize
 ##
-## parameters : 
-## vlist: RF Voltage Pattern
-## bline: pattern of bending magnet
+## @strong{Inputs}
+## @table @var
+## @item vlist
+## RF Voltage Pattern
+## @item bline
+## pattern of bending magnet
+## @item ws
+## synchrotron frequency in [rad/sec]
+## @end table
+##
+## @end deftypefn
 
 ##= History
+## 2011-01-25
+## * fixed conversion from [rad/sec] to [kHz]
+## 2011-???
 ## * reimplemented for Octave 3.2
 ## * renamed from plotPhaseOsillation
 
-function plot_phase_oscillation(tline,bline,bPoints_set,vlist,omega_s,phi_s,deltaEFromB,e_area, d_emax,nM,totalEnergy,phi1)
+function plot_phase_oscillation(tline,bline,bPoints_set,vlist,ws,phi_s,deltaEFromB,e_area, d_emax,nM,totalEnergy,phi1)
   # phi_s = ps;
   # deltaEFromB = d_e;
   # totalEnergy = Ee;
@@ -24,17 +39,17 @@ function plot_phase_oscillation(tline,bline,bPoints_set,vlist,omega_s,phi_s,delt
   bline_set = [tline, bline];
   vLine_set = [tline, vlist];
   phi_s_set = [tline, phi_s .*360./(2*pi)];
-  deltaEFromB_set = [tline, deltaEFromB/1000]; #[msec; keV]
-  d_emax_set = [tline, d_emax/1000]; #[msec; keV]
+  deltaEFromB_set = [tline, deltaEFromB*1e-3]; #[msec; keV]
+  d_emax_set = [tline, d_emax*1e-3]; #[msec; keV]
   e_area_set = [tline, e_area/10000]; #[msec; 10 keV]
-  omega_s_set = [tline, omega_s./1000];
+  ws_set = [tline, ws./(2*pi)*1e-3];
   # kEList = totalEnergy - nM*proton_eV; #Kinetic Energy
   kEList = totalEnergy - m0c2*1e6; #Kinetic Energy
 
   relative_e_area_set = [tline, (e_area./(kEList*4*pi)).*100]; #[msec; %]
 
   subplot(3,1,1);
-  ax = xyyplot({omega_s_set, "-;synchrotorn Frequency [KHz];"},...
+  ax = xyyplot({ws_set, "-;synchrotorn Frequency [KHz];"},...
                {phi_s_set, "-g;phase angle of synchronus particle [degree];"});...
   ylabel(ax(1), "synchrotorn Frequency [kHz]");...
   ylabel(ax(2), "phase angle [degree]");
