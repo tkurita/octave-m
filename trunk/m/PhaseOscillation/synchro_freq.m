166f93f4dbdb087ba53155020d6a11b18c203f4b
Tetsuro Kurita <tkurita@mac.com>
2011-01-25 12:49:15 +0000

    accept brho instead of BL
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@397 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/PhaseOscillation/synchro_freq.m
+++ trunk/m/PhaseOscillation/synchro_freq.m
@@ -1,29 +1,51 @@
-## シンクロトロン周波数を計算
-## alpha : momentum compaction factor;
-## particle: mass number or "proton", "helium", "carbon"
-## bLine : 磁場パターン
-## h : harmonic number
-## sihPhi_s : sin(phi_s) 
-## phi_s : synchronus particle の RF phase angle
-## vList : 電圧パターン
-## C : シンクロトロンの周長 [m]
+## -*- texinfo -*-
+## @deftypefn {Function File} {[@var{ws}, @var{Ee}, @var{eta} =} synchro_freq(@var{alpha}, @var{particle}, @var{brho}, @var{h}, @var{sin_ps}, @var{vline}, @var{C})
+## Calculate syncrotron frequency.
 ##
-## omega_s : シンクロトロン周波数[rad/sec]
-## Ee : 磁場に対応する全エネルギー
-## eta : alpha - 1/gamma^2 
-
+## @strong{Inputs}
+## @table @var
+## @item alpha
+## momentum compaction factor
+## @item particle
+## mass number or "proton", "helium", "carbon".
+## @item brho
+## BM pattern in [T*m]
+## @item h
+## harmonic number
+## @item sin_ps
+## sin(ps), ps : the RF phase angle of synchronus particle.
+## @item vline
+## RF voltage pattern in [V]
+## @item C
+## circumference [m]
+## @end table
+##
+## @strong{Outputs}
+## @table @var
+## @item ws
+## sinchrotoron frequency in [rad/sec]
+## @item Ee
+## total energy evaluated from @var{brho}
+## @item eta
+## alpha - 1/gamma^2 
+## @end table
+##
+## @end deftypefn
 
 ##== History
+## 2011-01-25
+## * accept brho instead of BL
+## * help with texinfo.
 ## 2010-12-21
 ## * renamed from synchroFrequency
 
-function [omega_s,Ee,eta] = synchro_freq(alpha, particle, bLine, h, sinPhi_s, vList,C)
+function [ws,Ee,eta] = synchro_freq(alpha, particle, brho, h, sin_ps, vline,C)
   lv = physical_constant("SPEED_OF_LIGHT_IN_VACUUM"); #光速
   m0c2 = mass_energy(particle)*1e6; #[eV]
-  Ee2 = m0c2^2 + (bLine./(pi/4)).^2 .*lv^2; #[ev2]
+  Ee2 = m0c2^2 + brho.^2 .*lv^2; #[ev2]
   Ee = sqrt(Ee2); #[eV]
   eta = alpha - m0c2^2./Ee2; #
-  cosphi = - sqrt(1 - sinPhi_s.^2);
+  cosphi = - sqrt(1 - sin_ps.^2);
   omegaC = 2 * pi*lv/C;
-  omega_s = sqrt((eta .* h .* omegaC^2 .*vList .* cosphi)./(2 * pi .* Ee));
+  ws = sqrt((eta .* h .* omegaC^2 .*vline .* cosphi)./(2 * pi .* Ee));
 endfunction
