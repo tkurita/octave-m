5605211df988acc8cffee00ba1dcd592d09a60d5
Tetsuro Kurita <tkurita@mac.com>
2011-01-25 13:21:26 +0000

    [Octave-f] fixed conversion from [rad/sec] to [kHz]
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@399 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/PhaseOscillation/plot_phase_oscillation2.m
+++ trunk/m/PhaseOscillation/plot_phase_oscillation2.m
@@ -1,9 +1,26 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {} plot_phase_oscillation2(@var{tline}, @var{bline}, @var{bpoints}, @var{vlist}, @var{ws}, @var{ps}, @var{e_area2})
 ## plot ...
-## synchrotron frequency and Phase angle of synchronus particle
-## bucket area in dR/(h omega)-phi coordinate
-## pattern of bending magnet and RF voltage
+## @itemize
+## @item synchrotron frequency and Phase angle of synchronus particle.
+## @item bucket area in dR/(h omega)-phi coordinate.
+## @item pattern of bending magnet and RF voltage.
+## @end itemize
+##
+## @strong{Inputs}
+## @table @var
+## @item tline
+## time [msec]
+## @item bline
+## BM pattern
+## @item bpoints
+## @end table
+##
+## @end deftypefn
 
 ##==History
+## 2011-01-25
+## * fixed conversion from [rad/sec] to [kHz]
 ## 2011-01-07
 ## * reimplemented for Octave 3.2
 ## * renamed from plotPhaseOsillation2.m
@@ -16,7 +33,7 @@ function plot_phase_oscillation2(tline,bline,bPoints_set,vlist,omega_s,phi_s,e_a
   vline_set = [tline,vlist];
   phi_s_set = [tline, phi_s .*360./(2*pi)];
   e_area2_set = [tline, e_area2] ; #[msec; 10 eV sec]
-  omega_s_set = [tline, omega_s./1000];
+  omega_s_set = [tline, omega_s*1e-3/(2*pi)];
   
   subplot(2,1,1);
   ax = xyyplot({omega_s_set, "-;synchrotorn Frequency [KHz];",...
