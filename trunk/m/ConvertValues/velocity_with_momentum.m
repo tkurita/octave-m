b5b2158f8a5d2ee9e8498e13c4db7966070a2127
Tetsuro Kurita <tkurita@mac.com>
2011-02-16 05:31:32 +0000

    [Octave-f] add velocity_with_momentum.m
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@407 a4934b6f-310c-0410-bed4-ac220e363503

---
+++ trunk/m/ConvertValues/velocity_with_momentum.m
@@ -0,0 +1,32 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{result} =} velocity_with_momentum(@var{p}, @var{particle}, @var{charge})
+##
+## @table @var
+## @item p
+## momentum [MeV/m]
+## @item particle
+## The kind of the particle. "proton", "helium", "carbon" or mass number.
+##
+## @item @var{charge}
+## charge number.
+## @end table
+##
+## @end deftypefn
+
+##== History
+## 2011-02-16
+## * First Implementation.
+
+function retval = velocity_with_momentum(p, particle)
+  if nargin < 2
+    print_usage();
+  endif
+  mass_e = mass_energy(particle);
+  lv = physical_constant("SPEED_OF_LIGHT_IN_VACUUM");
+  x = lv*p/mass_e; # beta/(sqrt(1-beta^2))
+  b2 = x.^2./(1+x.^2);
+  retval = sqrt(b2)*lv;
+endfunction
+
+%!test
+%! velocity_with_momentum(x)
