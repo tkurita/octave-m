ec3a56c0104a6b42222e99b05b867678c49191b1
Tetsuro Kurita <tkurita@mac.com>
2011-03-03 00:28:21 +0000

    [Octave-f] phase_margin.m : add errors "gain is under zero dB." and "gain is over zero dB."
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@411 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/beam-feedback/phase_margin.m
+++ trunk/m/beam-feedback/phase_margin.m
@@ -18,11 +18,19 @@
 ## @end deftypefn
 
 ##== History
+## 2011-03-03
+## * add errors "gain is under zero dB." and "gain is over zero dB."
 ## 2011-03-02
 ## * First implementation
 
 function retval = phase_margin(tfc)
   db = 20*log10(tfc.mag);
+  if max(db) < 0 
+    error("gain is under zero dB.");
+  endif
+  if min(db) > 0 
+    error("gain is over zero dB.");
+  endif
   w = tfc.w;
   w_db0 = interp1(db, w, 0);
   retval =interp1(w, tfc.phase, w_db0) + 180;
