ee6002a84b2bbf0b889be4aa774af970d103041d
Tetsuro Kurita <tkurita@mac.com>
2011-07-27 06:47:38 +0000

    [Octave-f] vline.m : first argument can be axes object
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@417 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/plot/vline.m
+++ trunk/m/plot/vline.m
@@ -7,12 +7,24 @@
 ## * properties -- line type. optional
 
 ##== History
+## 2011-07-27
+## * first argument can be axes object
 ## 2007-10-31
 ## * use line instead of __gnuplot_raw__
 ## * The code chekcing automatic_replot is removed for compatibility to 2.9.14
 
-function result = vline(x, varargin)
-  ca = gca();
+function result = vline(varargin)
+  if (ishandle(varargin{1}))
+    ca = varargin{1};
+    set(gcf, "currentaxes", ca);
+    varargin(1) = [];
+  else
+    ca = gca();
+  endif
+  
+  x = varargin{1};
+  varargin(1) = [];
+
   global __ylim;
   __ylim = get(ca, "ylim");
   global __prop;
@@ -23,7 +35,6 @@ endfunction
 function result = _vline(x)
   global __ylim;
   global __prop;
-  
   if (length(__prop) > 0)
     result = line([x,x], ylim, __prop{:});  
   else
