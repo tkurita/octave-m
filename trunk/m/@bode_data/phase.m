4435440798a6c867a27f51a5fa05e2c1da2916ca
Tetsuro Kurita <tkurita@mac.com>
2012-08-08 04:16:21 +0000

    bode_data.tf is allowed function name.
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@427 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/@bode_data/phase.m
+++ trunk/m/@bode_data/phase.m
@@ -16,6 +16,8 @@
 ## @end deftypefn
 
 ##== History
+## 2012-08-08
+## * bode_data.tf is allowed function name.
 ## 2012-07-17
 ## * first implementation
 
@@ -25,8 +27,8 @@ function retval = phase(X, varargin)
     params = join_struct(params, varargin{:});
   endif
   a_tf = X.tf;
-  if isa(a_tf, "function_handle")
-    a_tf = X.tf(params);
+  if isa(a_tf, "function_handle") || isa(a_tf, "char")
+    a_tf = feval(a_ft, params);
   endif
   [m,p,w] = bode(a_tf, 2*pi*X.f_in);
   retval = [w/(2*pi), p];
