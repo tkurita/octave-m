8128bd7932e65bd7232f3d50b557678caccaa0ce
Tetsuro Kurita <tkurita@mac.com>
2012-10-04 05:08:35 +0000

    use polyfit instead of wpolyfit.
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@429 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/Harmonics/ControlVoltage_Characteristics/HzToAmpControlV.m
+++ trunk/m/Harmonics/ControlVoltage_Characteristics/HzToAmpControlV.m
@@ -1,15 +1,34 @@
-## Usage : ctrlVList = HzToAmpControlV(hzList, A2_PM2)
-##          ２倍高調波信号処理装置の電圧設定の周波数特性
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{ctrlv} =} HzToAmpControlV(@var{Hz}, @var{A2_PM2})
+## Obtain control voltage from frequency characteristics of harmonics processing unit.
+## 
+## @strong{Inputs}
+## @table @var
+## @item Hz
+## list of frequency in Hz.
+## @item A2_PM2
+## a table of frequency characteristics
+## @end table
 ##
-## = Parameters
-## * hzlist -- [Hz]
-## * A2_PM2 -- 周波数特性データ
+## @strong{Outputs}
+## @table @var
+## @item ctrlv
+## control voltage
+## @end table
+##
+## @end deftypefn
+
+# ２倍高調波信号処理装置の電圧設定の周波数特性
+##== History
+## 2012-10-04
+## * use polyfit instead of wpolyfit.
 
 function ctrlVList = HzToAmpControlV(hzList, A2_PM2)
   #global A2_PM2;
   fHz = A2_PM2(:,2);
   A2 = A2_PM2(:,3);
-  [p, s, mu] = wpolyfit(fHz,A2,8);
+  #[p, s, mu] = wpolyfit(fHz,A2,8);
+  [p, s, mu] = polyfit(fHz,A2,8);
   ctrlVList = polyval(p,(hzList-mu(1))./mu(2));
   #y1 = polyval(p,(fHz-mu(1))./mu(2));
   #plot(fHz,A2,"",fHz,y,"")
