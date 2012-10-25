29e2528ff006894cb624f2e2efb7dbc4d946a2c1
Tetsuro Kurita <tkurita@mac.com>
2012-10-25 10:00:10 +0000

    improved document
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@439 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/xydata/y_at_x.m
+++ trunk/m/xydata/y_at_x.m
@@ -1,10 +1,15 @@
 ## -*- texinfo -*-
 ## @deftypefn {Function File} {@var{y1} =} y_at_x(@var{XY}, @var{x1})
 ##
-## Find y value near @var{x1}.
+## Evaluate y value at @var{x1} with linear interpetation.
+## 
 ## @var{xy} is two column matrix. 
 ##
 ## @end deftypefn
+
+##== History
+## 2012-10-25
+## * Improve document.
 
 function y = y_at_x(xydata, x)
   if (nargin < 2)
