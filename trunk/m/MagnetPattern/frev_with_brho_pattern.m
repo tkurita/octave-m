68e9d35abcc18e45f61bfb308dffa606d08e9dbc
Tetsuro Kurita <tkurita@mac.com>
2012-10-25 15:12:36 +0000

    remove harmonics number from aguments. return column wise vector.
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@444 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/MagnetPattern/frev_with_brho_pattern.m
+++ trunk/m/MagnetPattern/frev_with_brho_pattern.m
@@ -7,15 +7,13 @@
 ## @strong{Inputs}
 ## @table @var
 ## @item @var{brhos}
-## Pattern of B*rho value.
+## Pattern of B*rho [T*m] value.
 ## @item @var{partile}
 ## kind of particle.
 ## @item @var{q}
 ## charge number.
-## @item @var{h}
-## harmonics number.
 ## @item @var{f0}
-## caputure frequency.
+## caputure frequency [Hz].
 ## @end table
 ##
 ## @strong{Outputs}
@@ -27,6 +25,10 @@
 ## @end deftypefn
 
 ##== History
+## 2012-10-26
+## * remove harmonics number from aguments.
+## * return column wise vector.
+## 
 ## 2012-10-25
 ## * initial frev is f0.
 ## * frev has same length with brhos.
@@ -34,19 +36,19 @@
 ## 2012-10-25
 ## * added to shared function files.
 
-function frev = frev_with_brho_pattern(brhos, C, particle, q, h, f0)
+function frev = frev_with_brho_pattern(brhos, C, particle, q, f0)
 frev = [f0];
 pref = f0;
 m0c2 = mass_energy(particle)*1e6;
 lv = physical_constant("SPEED_OF_LIGHT_IN_VACUUM");
 dbrho = diff(brhos);
-
+h = 1;
 for n = 1:length(dbrho)
   bt = pref*C/(h*lv);
   gm = abs(1/sqrt(1-bt^2));
   df = (h/C)*q*lv^2/(m0c2 *(gm + bt^2*(1-bt^2)^(-3/2)))*dbrho(n);
   pref = pref+df;
-  frev(end+1) = pref;
+  frev(end+1,:) = pref;
 end
 endfunction
 
