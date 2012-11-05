53a5f23ea95942376f053ef07a29545ca8a0420f
Tetsuro Kurita <tkurita@mac.com>
2012-11-05 05:05:16 +0000

    change names due to specification chages of physical_constant.
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@451 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/ConvertValues/mass_energy.m
+++ trunk/m/ConvertValues/mass_energy.m
@@ -5,6 +5,8 @@
 ## * particle -- "proton" or "carbon" or "helium" or mass number [amu]
 
 ##== History
+## 2012-11-05
+## * change names due to specification chages of physical_constant.
 ## 2008-02-26
 ## * Use physical_constant intead of physicalConstant.
 ## * mass_energy can accelpt mass number
@@ -17,21 +19,23 @@ function result = mass_energy(particle)
   if (isnumeric(particle))
     switch particle
       case 1
-        result = physical_constant("PROTON_MASS_ENERGY_EQUIVALENT_IN_MEV");
+        result = physical_constant("proton mass energy equivalent in MeV");
+      case 2
+        result = physical_constant("deuteron mass energy equivalent in MeV");
       case 4
-        result = physical_constant("ALPHA_PARTICLE_MASS_ENERGY_EQUIVALENT_IN_MEV");
+        result = physical_constant("alpha particle mass energy equivalent in MeV");
       otherwise
-        amu = physical_constant("ATOMIC_MASS_CONSTANT_ENERGY_EQUIVALENT_IN_MEV");
+        amu = physical_constant("atomic mass constant energy equivalent in MeV");
         result = amu * particle;
     endswitch
   else
     switch particle
       case "proton"
-        result = physical_constant("PROTON_MASS_ENERGY_EQUIVALENT_IN_MEV");
+        result = physical_constant("proton mass energy equivalent in MeV");
       case "helium"
-         result = physical_constant("ALPHA_PARTICLE_MASS_ENERGY_EQUIVALENT_IN_MEV");
+         result = physical_constant("alpha particle mass energy equivalent in MeV");
       case "carbon"
-        amu = physical_constant("ATOMIC_MASS_CONSTANT_ENERGY_EQUIVALENT_IN_MEV");
+        amu = physical_constant("atomic mass constant energy equivalent in MeV");
         result = amu *12;
       otherwise
         error("\"%s\" is unknown particle specifier.", particle);
