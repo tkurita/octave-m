21114e0836c1d8456466400354bb465234c49970
Tetsuro Kurita <tkurita@mac.com>
2013-06-13 14:09:49 +0000

    * use arrayfun instead of map * remover persisitent _xlim in _hline
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@466 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/plot/hline.m
+++ trunk/m/plot/hline.m
@@ -3,51 +3,41 @@
 ## arguments
 
 ##== History
+## 2013-06-13
+## * use arrayfun instead of map
+## * remover persisitent _xlim in _hline
 ## 2011-07-27
 ## * first argument can accept axes object.
 ## 2008-03-10
 ## * reimplement fo version 3.0
 
 function hline(varargin)
-  if (ishandle(varargin{1}))
-    set(gcf, "currentaxes", varargin{1});
+  if (length(varargin) > 1 && ishandle(varargin{1}))
+    ca = varargin{1};
+    set(gcf, "currentaxes", ca);
     varargin(1) = [];
+  else
+    ca = gca();
   endif
-  ca = gca();
-  ##global __xlim;
-  _hline("xlim", get(ca, "xlim"));
   
   if (length(varargin) > 1)
     _hline("properties", varargin(2:end));
   end
-  result = map(@_hline, varargin{1});
+  result = arrayfun(@_hline, varargin{1});
 endfunction
 
 
 function result = _hline(varargin)
-  persistent _xlim;
   persistent _prop;
   
   if (ischar(varargin{1}))
-    [_xlim, _prop] = get_properties(varargin, {"xlim", "properties"}, {_xlim, _prop});
+    [_prop] = get_properties(varargin, {"properties"}, {_xlim, _prop});
     return;
   end
-  
   y = varargin{1};
   if (!isempty(_prop) > 0)
-    result = line(xlim, [y,y], _prop{:});  
+    result = line(xlim(), [y,y], _prop{:});  
   else
-    result = line( xlim, [y,y]);  
+    result = line(xlim(), [y,y]);  
   endif
 endfunction
-
-#function result = _hline(x)
-#  global __xlim;
-#  global __prop;
-#  
-#  if (length(__prop) > 0)
-#    result = line([x,x], xlim, __prop{:});  
-#  else
-#    result = line([x,x], xlim);  
-#  endif
-#endfunction
