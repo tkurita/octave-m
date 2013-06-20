c67610bcae4300e1da3516151c28155756ad4727
Tetsuro Kurita <tkurita@mac.com>
2013-06-20 10:05:33 +0000

    saved session file is commpresed with zip.
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@481 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/utils/save_session.m
+++ trunk/m/utils/save_session.m
@@ -1,6 +1,8 @@
 ## save all variables into the file "session-yyyymmdd.mat"
 
 ##== History
+## 2013-06-20
+## * saved session file is commpresed with zip.
 ## 2011-03-03
 ## * First Implementation
 
