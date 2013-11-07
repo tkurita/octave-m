a5bc8bde30413d39e563335cb2d52036e2045c42
Tetsuro Kurita <tkurita@mac.com>
2013-11-07 13:39:15 +0000

    pkg load io at beginning.
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@505 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/utils/cell/cell_with_csv.m
+++ trunk/m/utils/cell/cell_with_csv.m
@@ -4,9 +4,12 @@
 ## @end deftypefn
 
 ##== History
-##
+## 2013-11-07
+## * pkg load io at beginning.
 
 function retval = cell_with_csv(file)
+  pkg load io;
+
   retval = {};
   [fid, msg] = fopen(file, "r");
   if (fid == -1)
