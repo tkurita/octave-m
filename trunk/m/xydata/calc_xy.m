005aeec8345600fef3d35fc305864c9433de701d
Tetsuro Kurita <tkurita@mac.com>
2013-11-11 10:46:52 +0000

    support element by element operation.
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@508 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/xydata/calc_xy.m
+++ trunk/m/xydata/calc_xy.m
@@ -1,12 +1,13 @@
 ## -*- texinfo -*-
-## @deftypefn {Function File} {@var{result} =} calc_xy(@var{xy},"x+", @var{xshift}, "y+", @var{yshift}, "x*", @var{xfactor}, "y*", @var{yfactor})
+## @deftypefn {Function File} {@var{result} =} calc_xy(@var{xy},"x+", @var{xshift}, "y+", @var{yshift}, "x*", @var{xfactor}, "y*", @var{yfactor}, "x.*", @var{xvec}, "y.*", @var{yvec})
 ##
 ## @end deftypefn
 
 ##== History
+## 2013-11-11
+## * support element by element operation.
 ## 2009-06-09
 ## * If enough arguments are not given, print_usage().
-##
 ## 2009-05-20
 ## * First implementaion. Will replace factoryX/Y shift_xy
 
@@ -18,8 +19,9 @@ function xy = calc_xy(xy, varargin)
     print_usage();
   endif
   
-  [px, py, mx, my] = get_properties(varargin, ...
-                    {"x+", "y+", "x*", "y*"}, {NaN, NaN, NaN, NaN});
+  [px, py, mx, my, ebmx, ebmy] = get_properties(varargin, ...
+                    {"x+", "y+", "x*", "y*", "x.*", "y.*"},...
+                     {NaN, NaN, NaN, NaN, NaN, NaN});
   if (!isnan(px))
     xy(:,1) += px;
   endif
@@ -31,6 +33,12 @@ function xy = calc_xy(xy, varargin)
   endif
   if (!isnan(my))
     xy(:,2) *= my;
+  endif
+  if ! isnan(ebmx)
+    xy(:,1) .*= ebmx;
+  endif
+  if ! isnan(ebmy)
+    xy(:,2) .*= ebmy;
   endif
 endfunction
 
