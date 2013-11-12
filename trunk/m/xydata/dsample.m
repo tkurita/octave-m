9c7669b1582735e03d63d7bf0b0dc5de91943735
Tetsuro Kurita <tkurita@mac.com>
2013-11-12 02:08:56 +0000

    first implementation
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@509 a4934b6f-310c-0410-bed4-ac220e363503

---
+++ trunk/m/xydata/dsample.m
@@ -0,0 +1,44 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{retval} =} dsample(@var{mat}, @var{N})
+## down sample rows of @var{mat} until about @var{N}.
+## @strong{Inputs}
+## @table @var
+## @item mat
+## A row-wise matrix
+## @item N
+## Expected number of rows.
+## @end table
+##
+## @strong{Outputs}
+## @table @var
+## @item retval
+## A matrix whose rows are down sampled.
+## @end table
+##
+## @end deftypefn
+
+##== History
+## 2013-11-12
+## * first implementation
+
+function retval = dsample(mat, N)
+  if ! nargin
+    print_usage();
+  endif
+
+  n_rows = rows(mat);
+  if n_rows >= N
+    retval = mat;
+    return;
+  endif
+  
+  n_skip = round(n_rows/N);
+  if n_skip < 2
+    n_skip = 2;
+  endif
+  ir = 1:n_skip:n_rows;
+  retval = mat(ir, :);
+endfunction
+
+%!test
+%! func_name(x)
