df26dd69dbc257fd34318b350918239359429cd6
Tetsuro Kurita <tkurita@mac.com>
2014-01-10 00:43:35 +0000

    maximum and deviation will be displayed
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@513 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/equilibrium-charge/eq_charge_dist.m
+++ trunk/m/equilibrium-charge/eq_charge_dist.m
@@ -19,9 +19,11 @@
 ##
 ## Here is labels of options.
 ## @table @code
-## @item "minq"
-## @item "maxq"
-## @item "threshold"
+## @item stripper
+## "gas" or "carbon". 
+## @item minq
+## @item maxq
+## @item threshold
 ## @end table
 ## 
 ## Here is the structure of output.
@@ -33,6 +35,8 @@
 ## @end deftypefn
 
 ##== History
+## 2014-01-10
+## * maximum and deviation will be displayed.
 ## 2009-06-12
 ## * use Sayer's distribution. R.O.Sayer Review. de. Phys. App. 12(’77)1543
 ## 2009-06-11
@@ -63,7 +67,7 @@ function retval = eq_charge_dist(varargin)
       rho = 0.35*z^0.55*((q0/z)*(1-q0/z))^0.27;
       epsi = rho*(0.17+0.0012*z-3.3*b);
     case "carbon"
-      q0 = z*(1-1.03*exp(-47.3*z^(-0.38)*b^0.86))
+      q0 = z*(1-1.03*exp(-47.3*z^(-0.38)*b^0.86));
       rho = 0.48*z^0.45*((q0/z)*(1-q0/z))^0.26;
       epsi = rho*(0.0007*z-0.7*b);
     otherwise
@@ -88,9 +92,11 @@ function retval = eq_charge_dist(varargin)
     retval = [qlist(:), fracs(:)];
     retval(retval(:,2) <= opts.threshold, :) = [];
   else
+    printf("  maximum : %.3f\n", q0);
+    printf("deviation : %.3f\n", rho);
     for n=1:length(qlist)
       if (fracs(n) > opts.threshold)
-        printf("%2d+ : %.3f\n", qlist(n), fracs(n));
+    printf("      %2d+ : %.3f\n", qlist(n), fracs(n));
       endif
     endfor
   endif
