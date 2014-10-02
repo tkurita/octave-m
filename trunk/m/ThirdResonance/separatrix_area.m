c5a5d56a0e4caeb314343a55eb545c7779a35776
Tetsuro Kurita <tkurita@mac.com>
2014-10-02 06:59:50 +0000

    if nargout == 0, print the formatted result.
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@549 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/ThirdResonance/separatrix_area.m
+++ trunk/m/ThirdResonance/separatrix_area.m
@@ -11,15 +11,21 @@
 ## [pi mrad mm]
 
 ##== History
+## 2014-10-02
+## * if nargout == 0, print the formatted result.
 ## 2008-02-09
 ## * use values_for_separatrix instead of prepare_for_separatrix
-## 
 ## 2007-10-12
 ## * initial implementaion
 
-function area = separatrix_area(track_rec)
+function varargout = separatrix_area(track_rec)
   sep_info = values_for_separatrix(track_rec);
-  area = 2/sqrt(3)*(sep_info.delta_tune/sep_info.a_3n0)^2*1e6/pi;
+  sep_area = 2/sqrt(3)*(sep_info.delta_tune/sep_info.a_3n0)^2*1e6/pi;
+  if nargout 
+    varargout{1} = sep_area;
+  else
+    printf("%f [pi mrad mm]\n", sep_area);
+  endif
 endfunction
 
   
