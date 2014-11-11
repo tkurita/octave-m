3abf56dd2ba610f68be795c1eb774e9bef129d41
Tetsuro Kurita <tkurita@mac.com>
2014-11-11 09:28:01 +0000

    help description added.
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@557 a4934b6f-310c-0410-bed4-ac220e363503

---
+++ trunk/m/DegitalFilter/make_fir.m
@@ -0,0 +1,42 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{retval} =} make_fir(@var{N}, @var{fc}, @var{ftype})
+## make a FIR LPF or a FIT HPF. The result can be passed to filter or freqz function.
+## @strong{Inputs}
+## @table @var
+## @item N
+## order of the filder
+## @item fc
+## cutoff frequency
+## @item ftype
+## filter type. "low" : LPF or "high" : HPF.
+## @end table
+##
+## @end deftypefn
+
+##== History
+## 2014-11-11
+## * help description added.
+
+function varargout = make_fir(N, fc, ftype)
+  # fc : normalized frequency with sampling frequency
+  if ! nargin
+    print_usage();
+  endif
+  
+  switch ftype
+    case "high"
+      wc = pi *(1 - 2*fc);
+    otherwise # LPF
+    wc = 2*pi*fc;
+  endswitch
+  n = 0:N;
+  h = (wc/pi)*sinc(n*wc./pi);
+  switch ftype
+    case "high"
+      h = (-1).^n.*h;
+  endswitch
+  varargout{end+1} = h;
+endfunction
+
+%!test
+%! func_name(x)
