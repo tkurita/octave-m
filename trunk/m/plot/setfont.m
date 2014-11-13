f54337eaa0a66a275082b3b8f27418c407c866e7
Tetsuro Kurita <tkurita@mac.com>
2014-11-13 09:22:32 +0000

    support multiple axis in a figure.
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@570 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/plot/setfont.m
+++ trunk/m/plot/setfont.m
@@ -8,18 +8,27 @@
 ## @end deftypefn
 
 ##== History
+## 2014-11-13
+## * support multiple axis in a figure.
 ## 2010-07-29
 ## * First implementation
 
 function retval = setfont(varargin)
+  ax = findobj(gcf, "type", "axes");
   ##=== ticks label
-  set(gca, varargin{:}); # axis ticks label
+  #set(ax, varargin{:}); # axis ticks label
 
-  ##=== xlabel
-  xl = get(gca, "xlabel");
-  set(xl, varargin{:});
-
-  #=== ylabel
-  yl = get(gca, "ylabel");
-  set(yl, varargin{:});
+  ##=== xlabel, ylabel
+  for labelname = {"xlabel", "ylabel"}
+    lh = get(ax, labelname{1});
+    if iscell(lh)
+      lh = cell2mat(lh);
+    endif
+    label_handles = findobj(lh, "visible", "on");
+    if (length(label_handles) > 0)
+      set(label_handles, varargin{:});
+    endif
+  endfor
+  
+  set(findobj(gcf, "-property", "fontname"), varargin{:});
 endfunction
