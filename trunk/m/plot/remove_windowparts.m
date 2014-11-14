6bb18f149c73da967660eb5ee1bb21981131741d
Tetsuro Kurita <tkurita@mac.com>
2014-11-14 08:23:32 +0000

    fixed
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@579 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/plot/remove_windowparts.m
+++ trunk/m/plot/remove_windowparts.m
@@ -1,6 +1,7 @@
 ## -*- texinfo -*-
 ## @deftypefn {Function File} {@var{retval} =} remove_windowparts(@var{filename})
 ## remove window parts from a screen capture image.
+## ImageMagic is required.
 ##
 ## @end deftypefn
 
@@ -12,13 +13,11 @@ function retval = remove_windowparts(filename)
   if ! nargin
     print_usage();
   endif
-  [status, outtext] = system(["identify -format '%w %h' ", filename])
+  [status, outtext] = system(["identify -format '%w %h' ", filename]);
   wh = strsplit(outtext, " ");
   bottom_margin = 20;
   top_margin = 42;
   height = str2num(wh{2})-top_margin-bottom_margin;
-  printf("convert -crop %dx%d+%d+%d %s out.png\n", str2num(wh{1}), height,...
-                            0, bottom_margin, filename)
   [retval, outtext] = system(sprintf("convert -crop %dx%d+%d+%d %s %s",...
                      str2num(wh{1}), height, 0, top_margin, filename, filename));
 endfunction
