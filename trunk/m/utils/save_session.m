c80103a46e1111724b8c3acab5442510ca517913
Tetsuro Kurita <tkurita@mac.com>
2014-11-20 16:26:32 +0000

    use session_filename. append "-binary" option
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@586 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/utils/save_session.m
+++ trunk/m/utils/save_session.m
@@ -1,12 +1,14 @@
 ## save all variables into the file "session-yyyymmdd.mat"
 
 ##== History
+## 2014-11-20
+## * use session_filename
+## * append "-binary" option
 ## 2013-06-20
 ## * saved session file is commpresed with zip.
 ## 2011-03-03
 ## * First Implementation
 
-ssfilename = ["session-", strftime("%Y%m%d", localtime(time())), ".mat"];
-save("-z", ssfilename);
+save("-z","-binary", session_filename());
 
 disp(["success to save session into ", ssfilename]);
