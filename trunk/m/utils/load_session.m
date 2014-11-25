85a34b759062e5214d4713371b57aa7cef8e0834
Tetsuro Kurita <tkurita@mac.com>
2014-11-25 07:37:58 +0000

    added support of SESSION_ID"
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@588 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/utils/load_session.m
+++ trunk/m/utils/load_session.m
@@ -1,6 +1,8 @@
 ## load last session file
 
 ##== History
+## 2014-11-25
+## * added SESSION_ID support.
 ## 2014-11-17
 ## * fixed for 3.8.2.
 ## 2014-08-01
@@ -15,19 +17,19 @@
 1;
 
 function lastssfile = last_session_file(varargin)
-  persistent lastssfile = [];
+  lastssfile = [];
+  optname = [];
   if length(varargin)
-    return
-  else
-    lastssfile = [];  
+    optname = [varargin{1}, "-"];
   endif
   files = readdir("./");
   lastssfile = [];
   lastdate = 0;
+  namae_pattern =  ["session-", optname, "(\\d+)\\.mat"];
   for n = 1:length(files)
     # n = 13
     a_file = files{n};
-    [S, E, TE, M, T, NM] = regexp(a_file, "session-(\\d+)\\.mat");
+    [S, E, TE, M, T, NM] = regexp(a_file,namae_pattern);
     if S
       d = str2num(T{1}{1});
       if d > lastdate
@@ -39,11 +41,22 @@ function lastssfile = last_session_file(varargin)
 
   if isempty(lastssfile)
     error(["No session file in ", pwd]);
+    return false;
   endif
+  return true;
 endfunction
 
-load(last_session_file());
-if exist("session_init.m", "file")
-  session_init
+if exist("SESSION_ID", "var")
+  ssfilename = last_session_file(SESSION_ID);
+  init_file = ["session-", SESSION_ID, "-init.m"];
+else
+  ssfilename = last_session_file();
+  init_file = "session-init.m";
 endif
-disp(["Success to load ", last_session_file("filename")]);
+
+if load(ssfilename);
+  if exist(init_file, "file")
+    session_init
+  endif
+  disp(["Success to load ", ssfilename]);
+endif
--- trunk/m/utils/save_session.m
+++ trunk/m/utils/save_session.m
@@ -1,6 +1,8 @@
 ## save all variables into the file "session-yyyymmdd.mat"
 
 ##== History
+## 2014-11-25
+## * added SESSION_ID support.
 ## 2014-11-20
 ## * use session_filename
 ## * append "-binary" option
@@ -9,6 +11,10 @@
 ## 2011-03-03
 ## * First Implementation
 
-save("-z","-binary", session_filename());
-
+if exist("SESSION_ID")
+  ssfilename = session_filename(SESSION_ID);
+else
+  ssfilename = session_filename(SESSION_ID);
+endif
+save("-z","-binary", ssfilename);
 disp(["success to save session into ", ssfilename]);
