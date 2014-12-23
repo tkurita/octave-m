be5358dec89ac3aca5c97e478c6a048ff9302b91
Tetsuro Kurita <tkurita@mac.com>
2014-12-23 11:08:51 +0000

    added xy_in_xrange
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@610 a4934b6f-310c-0410-bed4-ac220e363503

---
+++ trunk/m/xydata/xy_in_xrange.m
@@ -0,0 +1,37 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{retval} =} xy_in_xrange(@var{xy}, @var{xmin}, @var{xmax})
+## @deftypefnx {Function File} {@var{retval} =} xy_in_xrange(@var{xy}, @var{xrange})
+## Obtain xy data between @var{xmin} and @var{xmax}.
+##
+## @strong{Inputs}
+## @table @var 
+## @item xrange
+## a matrix of [@var{xmin}, @var{xmax}]
+## @end table
+##
+## @end deftypefn
+
+##== History
+## 2014-12-23
+## * first implementation
+
+function retval = xy_in_xrange(xy, varargin)
+  if nargin <= 1
+    print_usage();
+  endif
+  
+  switch length(varargin)
+    case 1
+      xmin = varargin{1}(1);
+      xmax = varargin{1}(2);
+    otherwise
+      xmin = varargin{1};
+      xmax = varargin{2};
+  endswitch
+
+  x = xy(:,1);
+  retval = xy((x >= xmin) & (x <= xmax), :);
+endfunction
+
+%!test
+%! func_name(x)
