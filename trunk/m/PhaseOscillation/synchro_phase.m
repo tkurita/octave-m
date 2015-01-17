1fbe8aa57f8d8a77fc3e6776f969c59d321d6db9
Tetsuro Kurita <tkurita@mac.com>
2015-01-17 09:39:11 +0000

    some fixes.
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@614 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/PhaseOscillation/synchro_phase.m
+++ trunk/m/PhaseOscillation/synchro_phase.m
@@ -1,40 +1,33 @@
 ## -*- texinfo -*-
-## @deftypefn {Function File} {@var{ps}, @var{sin_ps} =} synchro_phase(@var{brho}, @var{tline}, @var{vline}, @var{C})
-## Calculate of phase of synchronus particle.
+## @deftypefn {Function File} {@var{ps}, @var{sin_ps} =} synchro_phase(@var{bl}, @var{tline}, @var{vline}, @var{C})
+## Calculate of phase of synchronous particle.
 ## 
 ## @strong{Inputs}
 ## @table @var
 ## @item brho
-## BM pattern in [T*m]
+## BM pattern in BL [T*m]
 ## @item tline
-## list of time in [msec]
+## list of time in [ms]
 ## @item vline
 ## RF voltage pattern [V]
 ## @item C
-## circumference [m]
+## circumference [m], 33.201 mm for WERC
 ## @end table
 ##
 ## @strong{Outputs}
 ## @table @var
 ## @item ps
-## phase angle in [rad]. always in 0 to pi/2 ignoring below and above transition.
+## phase angle in [rad].
+## always in 0 to pi/2 ignoring below and above transition.
 ## @item sin_ps
 ## sin(ps)
 ## @end table
 ##
 ## @end deftypefn
 
-
-## usage: [ps,sin_ps] = synchro_phase(brho,tline,vline,C)
-## synchronus particle の加速位相を計算
-## transition 前後、RFの位相と時間軸の関係等を無視して、0 - pi/2 の範囲で計算する。
-## bGrad : 磁場の変化率
-## vline : 電圧パターン
-## C : 周長 [m] WERC は 33.201mm
-
 ##== History
 ## 2011-01-25
-## * accept Brho instead of BL
+## * accept Brho instead of BL -- not correct
 ## * help with texinfo
 ## 2010-12-21
 ## * renamed from synchronusRFPhase
@@ -43,14 +36,12 @@
 ## 2009-10-30
 ## * It looks that gradient(tline/1000) is needed. gradient was changed ?
 
-function [ps,sin_ps] = synchro_phase(brho, tline, vline, C)
-  # brho = brho
-  # tline = tline
-  # vline = vline
+function [ps,sin_ps] = synchro_phase(bl, tline, vline, C)
   # C = 33.201
-  # bGrad=gradient(brho, gradient(tline/1000)); #time difference of BM magnetic field
-  bGrad=gradient(brho, (tline/1000));
-  sin_ps = (C*bGrad/(pi/4))./vline;
+  # A charge number as a parameter is not required,
+  # because it is cancled between bl and vline.
+  dbdt=gradient(bl, (tline/1000));
+  sin_ps = (C*dbdt/(pi/4))./vline;
   sin_ps(isnan(sin_ps)) = 0;
   ps =asin(sin_ps);
 endfunction
