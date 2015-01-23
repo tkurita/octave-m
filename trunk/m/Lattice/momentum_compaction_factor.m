f7ed9f4fb6ef1e5c9f55d49e93727891fc0ff5eb
Tetsuro Kurita <tkurita@mac.com>
2015-01-23 03:42:18 +0000

    append texinfo help.
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@619 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/Lattice/momentum_compaction_factor.m
+++ trunk/m/Lattice/momentum_compaction_factor.m
@@ -1,30 +1,42 @@
-## usage: alpha = momentum_compaction_factor(allElements)
-##
-## momentum compaction factor を計算する。
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{retval} =} momentum_compaction_factor(@var{lattice})
+## Obtain momentum compaction factor.
+## @strong{Inputs}
+## @table @var
+## @item lattice
+## a cell array of elements or a sturct which have allElements field.
+## dispersion values of all elements should be prepared.
+## @end table
 ## 
-##= Parameter
-##* allElements: すべての要素の matrix の配列。
-##              calcLattice によって分散（field eater）が計算されている必要がある。
+## @seealso(calc_lattice)
+## @end deftypefn
 
 ##== History
+## 2015-01-23
+## * append texinfo help.
 ## 2008-04-30
 ## * renamed from momentumCompactionFactor
 ## * Use is_BM intead of isBendingMagnet
 
-function alpha = momentum_compaction_factor(allElements)
-  ##calc mometum compaction factor
+function alpha = momentum_compaction_factor(lattice)
+  if nargin < 1
+    print_usage(); return;
+  endif
+  if isstruct(lattice)
+    lattice = lattice.allElements;
+  endif
   alpha = 0;
-  for n = 1:length(allElements)
-    theElement = allElements{n};
+  for n = 1:length(lattice)
+    an_elem = lattice{n};
     
-    if (is_BM(theElement))
-      theEater = (allElements{n-1}.eater(1)+theElement.eater(1))/2;
-      alpha += theEater*theElement.bmangle;
+    if (is_BM(an_elem))
+      eater = (lattice{n-1}.eater(1)+an_elem.eater(1))/2;
+      alpha += eater*an_elem.bmangle;
     endif
   end
   
   #C = 33.201; #peripheral length [m]
-  C = contourLength(allElements);
+  C = circumference(lattice);
   alpha /= C;
 endfunction
 
