f872c6bd2f32f6634ab842133ff7519b2066c542
Tetsuro Kurita <tkurita@mac.com>
2015-02-25 09:23:33 +0000

    first commit
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@657 a4934b6f-310c-0410-bed4-ac220e363503

---
+++ trunk/m/@fourier/change_at_idx.m
@@ -0,0 +1,23 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{fourier_obj} =} change_at_idx(@var{fourier_obj}, @var{idx}, @var{b})
+## Change fourier coefficients of index @var{idx} (1 based) to @var{b}.
+## @var{idx} must be less than < N/2.
+## 
+## @end deftypefn
+
+## $Date::                           $
+## $Rev$
+## $Author$
+
+##== History
+## 2015-02-25
+## * first implementation
+
+function x = change_at_idx(x, idx, b)
+  ns = length(x.fft_result);
+  x.fft_result(idx) = b;
+  x.fft_result(ns -idx + 2) = b;
+endfunction
+
+%!test
+%! func_name(x)
