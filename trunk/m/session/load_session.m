57d3d379a5b26dc868e3972aead4fff37f603bdc
Tetsuro Kurita <tkurita@mac.com>
2015-02-25 00:49:56 +0000

    reimplement as a function.
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@650 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/session/load_session.m
+++ trunk/m/session/load_session.m
@@ -1,12 +1,15 @@
 ## load last session file
 
 ##== History
+## 2015-02-25
+## * reimplement as a function.
 ## 2014-11-25
 ## * added SESSION_ID support.
 ## 2014-11-17
 ## * fixed for 3.8.2.
 ## 2014-08-01
-## * if "session_init.m" exists in load paths, the file will be preformed as a script file.
+## * if "session_init.m" exists in load paths, 
+##   the file will be preformed as a script file.
 ## 2013-10-25
 ## * A variable session_file is not used. 
 ##   Because if same name variable is included in the loaded session file, 
@@ -14,57 +17,36 @@
 ## 2011-03-03
 ## * First Implementaion
 
-1;
-
-function lastssfile = last_session_file(varargin)
-  lastssfile = [];
-  optname = [];
+function load_session(varargin)
+  ssid = [];
   if length(varargin)
-    optname = [varargin{1}, "-"];
+    ssid = varargin{1};
+  elseif evalin("caller", "exist SESSION_ID var")
+    ssid = evalin("caller", "SESSION_ID");
   endif
-  files = readdir("./");
-  lastssfile = [];
-  lastdate = 0;
-  namae_pattern =  ["session-", optname, "(\\d+)\\.mat"];
-  for n = 1:length(files)
-    # n = 13
-    a_file = files{n};
-    [S, E, TE, M, T, NM] = regexp(a_file,namae_pattern);
-    if S
-      d = str2num(T{1}{1});
-      if d > lastdate
-        lastdate = d;
-        lastssfile = a_file;
-      endif
-    endif
-  endfor
 
-  if isempty(lastssfile)
-    error(["No session file in ", pwd]);
+  if isempty(ssid)
+    ssfilename = last_session_file();
+    init_file = "session-init.m";
+  else
+    ssfilename = last_session_file(ssid);
+    init_file = ["session-", ssid, "-init.m"];
+  endif
+  
+  try
+    evalin("caller", sprintf("load '%s'", ssfilename))
+    disp(["Success to load ", ssfilename]);
+  catch
+    disp(["Failed to load ", ssfilename]);
     return;
+  end_try_catch
+  
+  if exist(init_file, "file")
+    try
+      source(init_file);
+      disp(["Success to evaluate ", init_file]);
+    catch
+      disp(["Failed to evaluate ", init_file]);
+    end_try_catch
   endif
 endfunction
-
-if exist("SESSION_ID", "var")
-  ssfilename = last_session_file(SESSION_ID);
-  init_file = ["session-", SESSION_ID, "-init.m"];
-else
-  ssfilename = last_session_file();
-  init_file = "session-init.m";
-endif
-
-try
-  load(ssfilename);
-  disp(["Success to load ", ssfilename]);
-catch
-  disp(["Failed to load ", ssfilename]);
-end_try_catch
-
-if exist(init_file, "file")
-  try
-    source(init_file);
-    disp(["Success to evaluate ", init_file]);
-  catch
-    disp(["Failed to evaluate ", init_file]);
-  end_try_catch
-endif
