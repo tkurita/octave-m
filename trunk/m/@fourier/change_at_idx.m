580e9809082a8a9603039030b418f349272c1ada
Tetsuro Kurita <tkurita@mac.com>
2015-02-25 09:25:55 +0000

    improved help
    git-svn-id: file:///Users/tkurita/Workspace/git-work/octave-repos/Octave-f@658 a4934b6f-310c-0410-bed4-ac220e363503

--- trunk/m/@fourier/change_at_idx.m
+++ trunk/m/@fourier/change_at_idx.m
@@ -2,6 +2,7 @@
 ## @deftypefn {Function File} {@var{fourier_obj} =} change_at_idx(@var{fourier_obj}, @var{idx}, @var{b})
 ## Change fourier coefficients of index @var{idx} (1 based) to @var{b}.
 ## @var{idx} must be less than < N/2.
+## The fourier cofficients of index (N -@var{idx}+2} is also set to @var{b}.
 ## 
 ## @end deftypefn
 
