128855b907a31dada45172e584f97fabffaac29a
Tetsuro Kurita <tkurita@mac.com>
2015-04-24 21:58:22 +0900

    add shift_xlim.m

---
+++ m/plot/shift_xlim.m
@@ -0,0 +1,24 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{retval} =} shift_xlim(@var{dx})
+## Shift the limits of the x-axis for the current plot.
+## equivalent to xlim(xlim() + @var{dx})
+##
+## @end deftypefn
+
+## $Date::                           $
+## $Rev$
+## $Author$
+
+##== History
+## 2015-04-24
+## * first implementation
+
+function retval = shift_xlim(dx)
+  if ! nargin
+    print_usage();
+  endif
+  retval = xlim(xlim() + dx);
+endfunction
+
+%!test
+%! func_name(x)
