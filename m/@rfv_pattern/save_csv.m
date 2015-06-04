b721bfa4e16b11b38db6bf43e754eec2f5ff9cca
Tetsuro Kurita <tkurita@mac.com>
2015-06-04 22:54:39 +0900

    improved document

--- m/@rfv_pattern/save_csv.m
+++ m/@rfv_pattern/save_csv.m
@@ -1,26 +1,26 @@
 ## -*- texinfo -*-
-## @deftypefn {Function File} {@var{retval} =} save_csv(@var{rfv}, @var{output})
-## description
+## @deftypefn {Function File} {@var{retval} =} save_csv(@var{rfvp}, @var{outfile})
+## Write into a csv file for the input of the Sankosha function generator.
 ## @strong{Inputs}
 ## @table @var
-## @item rfv
+## @item rfvp
 ## description of @var{arg}
 ## @end table
 ##
 ## @strong{Outputs}
 ## @table @var
 ## @item retval
-## description of @var{retval}
+## output data written in @var{outfile}
 ## @end table
 ##
 ## @end deftypefn
 
-function retval = save_csv(rfv, outfile, varargout)
+function retval = save_csv(rfvp, outfile, varargout)
   if ! nargin
     print_usage();
   endif
   fs = 10e3; # サンプリング周波数 [Hz] 三光社 F.G.
-  v_pattern = vs_time(rfv, "frequency", fs);
+  v_pattern = vs_time(rfvp, "frequency", fs);
   retval = bit_with_control_v(control_v_for_rf_amplitude(v_pattern));
   csvwrite(outfile, retval(:))
 endfunction
