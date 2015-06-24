4da526117b9fbb42f13d34a8951162b588cf5cc3
Tetsuro Kurita <tkurita@mac.com>
2015-06-24 09:40:26 +0900

    added synchro_phase_with_frev

---
+++ m/PhaseOscillation/synchro_phase_with_frev.m
@@ -0,0 +1,39 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{ps} =} synchro_phase_with_frev(@var{frev}, @var{rfv}, @var{tsec}, @var{C}, @var{particle}, @var{q})
+## Evaluate synchronus phase with revolution frequency.
+## @strong{Inputs}
+## @table @var
+## @item frev
+## revolution frequency. The hamonics is assumed as 1.
+## Therefor @var{frev} should be RF frequency/harmonics.
+## 
+## @item rfv
+## RF voltage pattern.
+## @item tmsec
+## time in seconds.
+## @item C
+## circumference [m], 33.201 m for WERC
+## @end table
+##
+## @strong{Outputs}
+## @table @var
+## @item ps
+## synchronus phase in radian.
+## @end table
+##
+## @end deftypefn
+
+function [ps, sin_ps] = synchro_phase_with_frev(frev, rfv, tsec, C, particle, q)
+  if ! nargin
+    print_usage();
+  endif
+  dfdt = gradient(frev, tsec);
+  m0c2 = mass_energy(particle)*1e6;
+  lv = physical_constant("speed of light in vacuum");
+  sin_ps = (m0c2./(q*rfv)).*(1- (C*frev/lv).^2).^(-3/2)*(C/lv)^2.*dfdt;
+  sin_ps(isnan(sin_ps)) = 0;
+  ps = asin(sin_ps);
+endfunction
+
+%!test
+%! synchro_phase_with_frev(x)
--- m/xydata/moving_average.m
+++
@@ -1,8 +0,0 @@
-function retval = moving_average(xy, n)
-  x = xy(:,1);
-  y = xy(:,2);
-  maf1 = ones(n,1)./n;
-  maf2 = normalize_filter(hamming(n).*maf1, 0);
-  y2 = filter(maf2, 1, y);
-  retval = [x(n:end), y2(n:end)];
-endfunction
