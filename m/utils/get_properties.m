26fa48c3a888b0e715acb2a6a4e5c45e7d5d80ec
Tetsuro Kurita <tkurita@mac.com>
2016-02-11 00:32:09 +0900

    added texinfo, second argument accept propname/defval table

--- m/utils/get_properties.m
+++ m/utils/get_properties.m
@@ -1,8 +1,43 @@
-## Usage :
-##  [val1, val2, ...] = get_properties({"prop_a", val_a,..},
-##                                      {"prop_a",...},
-##                                      {def_val_a,...})
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{prop_values} =} get_properties(@var{params}, @var{prop_defvals})
+## @deftypefnx {Function File} {@var{prop_values} =} get_properties(@var{params}, @var{prop_names}, @var{def_values})
 ##
+## parse cell array of property/values pairs.
+## 
+## @example
+## x = get_properties(@{"prop_a", val_a, "switch_b", "prob_c",..@},
+##                       @{"prop_a"  , def_val_a; ...
+##                         "switch_b", boolean  ; ...
+##                         "flag_c", def_val_c @})
+##
+##    or
+##
+## x = get_properties(@{"prop_a", val_a, "switch_b", "prob_c", ...@},
+##                       @{"prop_a", "switch_b","flag_c", ...@},
+##                       @{def_val_a, boolean, def_val_c, ...@})
+## @end example
+## 
+## @strong{Inputs}
+## @table @var
+## @item params
+## cell array. usually varargin
+## @item labels
+## cell array of names of properties.
+## @item def_values
+## cell array of default value list.
+## If defualt value is boolean, the property can tread as a flag.
+## IF the flag property name is in @var{params}, the value will be treatead as true.
+## @end table
+##
+## @strong{Outputs}
+## @table @var
+## @item prop_values
+## If nargout is 1, @var{prop_values} will be a structure of which fields are @var{propnames}.
+## If nargout is greater than 1, @var{prp_values} will be a cell array of values.
+## @end table
+## 
+## @seealso{parseparam, inputParser}
+## @end deftypefn
 
 ##== History
 ## 2012-07-20
@@ -14,7 +49,15 @@
 ## * property name が指定されているだけの property にも対応した
 ## * その property が存在していれば true
 
-function varargout = get_properties(prop_list, prop_names, default_values)
+function varargout = get_properties(prop_list, varargin)
+  length(varargin)
+  if (length(varargin) == 1)
+    prop_names = varargin{1}(:,1)';
+    default_values = varargin{1}(:,2)';
+  else
+    prop_names = varargin{1};
+    default_values = varargin{2};
+  endif
   varargout = default_values;
   n_args = length(prop_list);        
   n = 1;
