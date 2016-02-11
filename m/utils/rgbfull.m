45b730beb7633f16d33380811162473cbd0191d9
Tetsuro Kurita <tkurita@mac.com>
2016-02-11 15:59:20 +0900

    added colorgradient.m form image package because image-oct382 can't not be installed through fink.

--- m/utils/rgbfull.m
+++ m/utils/rgbfull.m
@@ -1,4 +1,6 @@
 function result = rgbfull(n_colors)
-  pkg load image
+  # pkg load image
+  # 2015-11-09 image-oct382 can't not be installed.
+  # only colorgradient is copied.
   result = colorgradient([1,0,0,; 1,1,0; 0,1,0 ;0,1,1; 0,0,1; 1,0,1], n_colors);
 endfunction
