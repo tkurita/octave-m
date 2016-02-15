63b6c1ccd66c0764c2f33f0f747c203fd2cd0a64
Tetsuro Kurita <tkurita@mac.com>
2016-02-15 18:48:08 +0900

    fixed verbose equation

--- m/ConvertValues/dpdt_with_frev_pattern.m
+++ m/ConvertValues/dpdt_with_frev_pattern.m
@@ -1,5 +1,5 @@
 ## -*- texinfo -*-
-## @deftypefn {Function File} {@var{dpdt} =} dpdt_with_frev_pattern(@var{frev_t}, @var{a}, @var{C}, @var{particle})
+## @deftypefn {Function File} {@var{dpdt} =} dpdt_with_frev_pattern(@var{frev_t}, @var{C}, @var{particle})
 ##
 ## evaluate dp/dt 
 ##
@@ -7,8 +7,6 @@
 ## @table @var
 ## @item frev_t
 ## two column matrix of [sec, frev]
-## @item a
-## mementum compaction factor.
 ## @item C
 ## circumference in [m]
 ## @item particle
@@ -25,16 +23,15 @@
 ## @seealso{/Users/tkurita/WorkSpace/Synchrotron/2015-03 Bunch振動/0604/周回周波数の変化から運動量の変化を求める/df-to-dp.pdf}
 ## @end deftypefn
 
-function dpdt = dpdt_with_frev_pattern(frev_t, a, C ,particle)
+function dpdt = dpdt_with_frev_pattern(frev_t, C ,particle)
   if ! nargin
     print_usage();
   endif
   lv = physical_constant("speed of light in vacuum");
   b = C.*frev_t(:,2)./lv;
-  g = 1./sqrt(1-b.^2);
   m0c2 = mass_energy(particle);
   dfdt = gradient(frev_t(:,2), frev_t(:,1));
-  dpdt = (m0c2/lv^2)*C.*(g + b.^2.*(1 - b.^2).^(-3/2)).*dfdt;
+  dpdt = (m0c2/lv^2)*C.*((1 - b.^2).^(-3/2)).*dfdt;
   dpdt = [frev_t(:,1), dpdt];
 endfunction
 
