6207d242e7ca1d0fc4281e40652c99b1e8e02d83
Tetsuro Kurita <tkurita@mac.com>
2016-04-07 12:10:20 +0900

    use !iscell instead of ismatrix, because ismatrix returns true even if a cell array is passed in 4.0.0

--- m/struct/struct_fields_values.m
+++ m/struct/struct_fields_values.m
@@ -15,15 +15,11 @@
 ## a structure
 ## @end deftypefn
 
-## 2014-11-04
-## * fixed an error on 3.8.2.
-
 function s = struct_fields_values(names, fieldvalues)
-  if (ismatrix(fieldvalues))
+  if (! iscell(fieldvalues))
     fieldvalues = mat2cell(fieldvalues...
       , columns(fieldvalues), ones(1, rows(fieldvalues)));
   end
-  
   for n = 1:length(names)
      s.(names{n}) = fieldvalues{n};
   end
