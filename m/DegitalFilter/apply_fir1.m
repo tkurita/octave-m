070a8f382964ac71468a1e2218b1dec8d13eeb43
Tetsuro Kurita <tkurita@mac.com>
2016-04-25 17:04:47 +0900

    added apply_fir1

---
+++ m/DegitalFilter/apply_fir1.m
@@ -0,0 +1,23 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{retval} =} apply_fir1(@var{n}, @var{fcn}, @var{v})
+## make a fir filter using @var{n} and @var{fcn} and appy it to @var{v}.
+## @strong{Inputs}
+## @table @var
+## @item n
+## filter order
+## @item fcn
+## normalized cut off frequency in pi rad/sample.
+## 2*ts*fc , fc [Hz]
+## @item v
+## values to apply filter. 
+## @end table
+##
+## @end deftypefn
+
+function retval = apply_fir1(n, fcn, v);
+  fir = fir1(n, fcn);
+  retval = filter(fir, 1, v);
+endfunction
+
+%!test
+%! func_name(x)
