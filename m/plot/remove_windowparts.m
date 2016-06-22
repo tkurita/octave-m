99f97d1abb15afcfc591e7148cbfcd4d6985d9b9
Tetsuro Kurita <tkurita@mac.com>
2016-06-22 22:24:35 +0900

    qt graphics_toolkit is supported

--- m/plot/remove_windowparts.m
+++ m/plot/remove_windowparts.m
@@ -11,14 +11,23 @@ function retval = remove_windowparts(filename)
   endif
   [status, outtext] = system(["identify -format '%w %h' ", filename]);
   wh = strsplit(outtext, " ");
-  bottom_margin = 22;
-  top_margin = 47;
+  switch graphics_toolkit
+    case "fltk"
+      bottom_margin = 22;
+      top_margin = 47;
+    case "qt"
+      top_margin = 66;
+      bottom_margin = 0;
+    otherwise
+      error([graphics_toolkit, " is not supprted."]);
+  endswitch
+  
   is_retina = system("/usr/sbin/system_profiler SPDisplaysDataType | grep 'Display Type: Retina'  > /dev/null 2>&1");
   if (is_retina == 0)
     bottom_margin *= 2;
     top_margin *=2;
   endif
-  height = str2num(wh{2})-top_margin-bottom_margin;
+  height = str2num(wh{2})- top_margin - bottom_margin;
   [retval, outtext] = system(sprintf("convert -crop %dx%d+%d+%d %s %s",...
                      str2num(wh{1}), height, 0, top_margin, filename, filename));
 endfunction
