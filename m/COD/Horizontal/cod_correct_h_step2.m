c9d8f30f06d48c690da962d3e286ba2b661cad9e
Tetsuro Kurita <tkurita@mac.com>
2016-12-27 19:42:22 +0900

    added positions option. element names will be appended on the plot.

--- m/COD/Horizontal/cod_correct_h_step2.m
+++ m/COD/Horizontal/cod_correct_h_step2.m
@@ -1,4 +1,4 @@
-## Usage : cod_rec = cod_correct_h_step2(cod_rec, codAtBPM)
+## Usage : cod_rec = cod_correct_h_step2(cod_rec, codAtBPM, ["positions", positions])
 ##  - evaluate COD correction by step1
 ##  - obtain factored steererValues 
 ##    with comparing result of step1 and expected COD
@@ -6,19 +6,9 @@
 ##== Parameters
 ## * cod_rec : result of step1
 ## * codAtBPM : mesuared COD applying steerer values obtained by step1
+## * positions : y values for labels of element names.
 
-##== History
-## 2009-07-14
-## * add kickFactor into returned structure.
-## * steererValues field of returnd structure is updated with following kickFactor.
-##
-## 2007.09.05
-## * add expectedCOD into returend structure
-##
-## 2007.???
-## *initial
-
-function cod_rec = cod_correct_h_step2(cod_rec, codAtBPM)
+function cod_rec = cod_correct_h_step2(cod_rec, codAtBPM, varargin)
   
   # codAtBPM = cod_hold_data("../data0908/COD0908-210849.csv", "FB");
   cod_step1 = rmfield(cod_rec, "prediction");
@@ -29,20 +19,28 @@ function cod_rec = cod_correct_h_step2(cod_rec, codAtBPM)
 #  expected_cod(:,2) =  cod_rec.prediction.correctCOD(:,2) - cod_rec.correctCOD(:,2);
 #  cod_rec.expectedCOD = expected_cod;
   
-  
+  opts = get_properties(varargin ...
+                        , {"positions", [0.5,-1,-2.5]});
+
   xyplot(cod_rec.targetCOD,"@-;measured COD;" ...
     , cod_rec.correctCOD,"-;result of least mean squre fit;" ...
     , cod_rec.prediction.correctCOD,"-;COD prediction;" ...
     , setCODOriginAt(cod_rec.expectedCOD, cod_rec.lattice, "BPM7") ...
     , "-;expected COD after correction;" ...
-    , setCODOriginAt(cod_step1.targetCOD,cod_rec.lattice, "BPM7") ...
-    , "-@;COD after correction;"); ...
-  xlabel("position [m]");ylabel("COD [mm]");grid on;
+    , setCODOriginAt(cod_step1.targetCOD, cod_rec.lattice, "BPM7") ...
+    , "-@;COD after correction;" ...
+  ); ...
+  xlabel("position [m]");ylabel("COD [mm]");grid on;xlim([0, 33.2]);
+  elements_on_plot({"^BPM\\d*$"}, cod_rec.lattice...
+              , "yposition", sprintf("first %f", opts.positions(1)));
+  elements_on_plot({"BMPe1$", "BMPe2$", "STH*"}, cod_rec.lattice...
+              , "yposition", sprintf("first %f", opts.positions(2)));
+  elements_on_plot({"ESD1","^SX\\d$"}, cod_rec.lattice...
+              ,"yposition", sprintf("first %f", opts.positions(3)));
   
   cod_diff.codAtBPM = subtract_cod(cod_rec.codAtBPM, codAtBPM);
   cod_diff.targetCOD = cod_list_with_bpms(cod_diff);
   cod_diff = lFitCODWithFactor(cod_diff);
-  cod_diff.kickFactor
   cod_diff.steererValues = cod_rec.steererValues/cod_diff.kickFactor;
   disp_kicker_values(cod_diff);
   cod_rec.kickFactor = cod_diff.kickFactor;
