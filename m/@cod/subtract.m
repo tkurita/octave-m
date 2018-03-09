3b29ae1d06811c710959ff4e16cb2d46b5f7ba67
Tetsuro Kurita <tkurita@mac.com>
2018-03-09 11:02:16 +0900

    added a function to substrat kick angles.

--- m/@cod/subtract.m
+++ m/@cod/subtract.m
@@ -1,6 +1,8 @@
 ## -*- texinfo -*-
 ## @deftypefn {Function File} {@var{retval} =} subtract(@var{cod1}, @var{cod2})
-## cod1.at_bpms - cod2.at_bpms
+##  cod1.at_bpms - cod2.at_bpms
+## or
+##  cod1.kick_angles  - cod2.kick_angles
 ## @strong{Inputs}
 ## @table @var
 ## @item arg1
@@ -17,12 +19,22 @@
 ## @end deftypefn
 
 ##== History
-## * 2013-11-26
-## first implementation
+## 2018-02-09
+## * added a function to substrat kick angles.
+## 2013-11-26
+## * first implementation
 
 function cod1 = subtract(cod1, cod2)
-  
-  for [val, key] = cod2.at_bpms
-    cod1.at_bpms.(key) -= val;
-  endfor
+  if isstruct(cod1.at_bpms)
+    for [val, key] = cod2.at_bpms
+      cod1.at_bpms.(key) -= val;
+    endfor
+  else
+    for n = 1:length(cod1.kickers)
+      if ! strcmp(cod1.kickers, cod2.kickers)
+        error("kickers are not equivalent.");
+      endif
+    endfor
+    cod1.kick_angles -= cod2.kick_angles;
+  endif
 endfunction
