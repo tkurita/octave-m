f8d7ba6b6f55ead33d168493ce8472a0f14ab59b
Tetsuro Kurita <tkurita@mac.com>
2018-09-05 17:57:50 +0900

    added help description

--- m/utils/find_low.m
+++ m/utils/find_low.m
@@ -1,10 +1,11 @@
-## -*- texinfo -*-
-## @deftypefn {Function File} {@var{result} =} func_name(@var{arg})
-##
-## @end deftypefn
-
-##== History
-##
+# low_value = find_low(ylist, ["bin", bin, "threshold", threshold])
+#
+# ylist のベースラインを求める。
+# ylist のヒストグラムを作成し、'threshold' 以上の度数の最初の y をベースラインとする。
+# 
+# - bin : ヒストグラムの総ビン数
+# - threshold : 'threshold' 以上の最初のビンの値を 'low_value' とする。
+#
 
 function retval = find_low(ylist, varargin)
   [hbin, threshold] = get_properties(varargin, {"bin", "threshold"}, {200, 5});
