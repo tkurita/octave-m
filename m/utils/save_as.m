1bda94cac71b8e914fb2874d2af01254fb6bc28a
Tetsuro Kurita <tkurita@mac.com>
2018-09-19 14:00:18 +0900

    improved help description

--- m/utils/save_as.m
+++ m/utils/save_as.m
@@ -2,10 +2,9 @@
 ## @deftypefn {Function File} {} save_as(@var{value}, @var{variable_name})
 ## 
 ## Save the value @var{value} as @var{variable_name} into the file "@var{variable_name}.mat"
-## @end deftypefn
-
-##== History
 ##
+## @seealso{load_vars}
+## @end deftypefn
 
 function save_as(val, varname)
   eval([varname, "= val;"]);
