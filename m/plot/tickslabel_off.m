e4c6b195efe0338948abf3b0fd392ae57ffab98d
Tetsuro Kurita <tkurita@mac.com>
2019-06-13 17:23:11 +0900

    tickslabel_off accept both of "x", "y" at once

--- m/plot/tickslabel_off.m
+++ m/plot/tickslabel_off.m
@@ -4,13 +4,13 @@
 ##
 ## Hide label of ticks.
 ##
+## @strong{Example}
+## @example
+## plot(x);
+## tickslabel_off("x", "y")
+## @end example
+##
 ## @end deftypefn
-
-##== History
-## 2014-12-10
-## * not to access "*tick" property.
-## 2011-01-06
-## * First implementation
 
 function retval = tickslabel_off(varargin)
   if ishandle(varargin{1}) 
@@ -20,13 +20,15 @@ function retval = tickslabel_off(varargin)
     ax = gca;
   endif
   
-  axspec = varargin{1};
-  tickname = [axspec, "tick"];
-  ticklabel = [axspec, "ticklabel"];
-  
-  # current_ticks = get(ax, tickname);
-  set(ax, ticklabel, {});
-  #set(ax, tickname, current_ticks);
+  for n = 1:length(varargin)
+    axspec = varargin{n};
+    tickname = [axspec, "tick"];
+    ticklabel = [axspec, "ticklabel"];
+    
+    # current_ticks = get(ax, tickname);
+    set(ax, ticklabel, {});
+    #set(ax, tickname, current_ticks);
+  endfor
 endfunction
 
 %!test
