277818d6593a34439b5f256f0999a2cbbca371ac
Tetsuro Kurita <tkurita@mac.com>
2019-11-05 10:41:22 +0900

    reimplement using plotyy and stacke_plot

--- m/PhaseOscillation/plot_phase_oscillation2.m
+++ m/PhaseOscillation/plot_phase_oscillation2.m
@@ -18,33 +18,23 @@
 ##
 ## @end deftypefn
 
-##==History
-## 2011-01-25
-## * fixed conversion from [rad/sec] to [kHz]
-## 2011-01-07
-## * reimplemented for Octave 3.2
-## * renamed from plotPhaseOsillation2.m
-
 function plot_phase_oscillation2(tline,bline,bPoints_set,vlist,omega_s,phi_s,e_area2)
   # bPoints_set = bp;
   # phi_s = ps;
-  #   
-  bline_set = [tline,bline];
-  vline_set = [tline,vlist];
-  phi_s_set = [tline, phi_s .*360./(2*pi)];
-  e_area2_set = [tline, e_area2] ; #[msec; 10 eV sec]
-  omega_s_set = [tline, omega_s*1e-3/(2*pi)];
-  
-  subplot(2,1,1);
-  ax = xyyplot({omega_s_set, "-;synchrotorn frequency [KHz];",...
-                phi_s_set, "-;phase angle of synchronus particle [degree];"},...
-                {e_area2_set, "-r;bucket Area;"});...
-  ylabel(ax(1), "synchrotorn Frequency [KHz]\nphase angle [degree]");...
+  stacked_plot(2,1);
+  ax = plotyy(tline, [omega_s*1e-3/(2*pi), phi_s .*360./(2*pi)] ...
+              , tline, e_area2);
+  ylabel(ax(1), "synchrotorn Frequency [KHz]\nphase angle [degree]");
   ylabel(ax(2), "RF bucket area [eV*sec]");
-  subplot(2,1,2);
-  ax = xyyplot({bline_set, "-;BM Pattern;", bPoints_set, "*"},...
-               {vline_set, "-g;RF Voltage;"});...
-  ylabel(ax(1), "BL value of BM [T*m]");...
-  ylabel(ax(2), "RF Voltage [V]");...
-  ylim(ax(2), [0,450]);xlabel("Time [msec]");
+  legend("synchrotorn frequency [KHz]" ...
+        , "phase angle of synchronus particle [degree]" ...
+        , "bucket Area");
+  tickslabel_off(ax, "x");
+  stacked_plot(2,2);
+  ax = plotyy(tline, bline ...
+              , tline, vlist);
+  ylabel(ax(1), "BL value of BM [T*m]");
+  ylabel(ax(2), "RF Voltage [V]");
+  legend("BM Pattern", "RF Voltage");
+  ylim(ax(2), [0,450]);xlabel("Time [ms]");
 endfunction
