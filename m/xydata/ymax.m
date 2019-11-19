d3138d418dc7eda477a782543d243e56bcbd8e21
Tetsuro Kurita <tkurita@mac.com>
2019-11-19 13:53:19 +0900

    added support of xy struct

--- m/xydata/ymax.m
+++ m/xydata/ymax.m
@@ -10,6 +10,13 @@ function varargout = ymax(xy, varargin)
     print_usage();
     return;
   endif
+  if isstruct(xy)
+    x = xy.x;
+    y = xy.y;
+  else
+    x = xy(:,1);
+    y = xy(:,2);
+  endif
   
   if (length(varargin))
     opt = get_properties(varargin, {"xrange"}, {NaN});
@@ -17,18 +24,19 @@ function varargout = ymax(xy, varargin)
     if (isnan(xr))
       error("Unknown options");
     endif
-    x = xy(:,1);
     sxi = find(x >= xr(1))(1);
     exi = find(x > xr(2))(1);
-    xy2 = xy(sxi:exi,:);
-    [max_data, iy] = ymax(xy2);
+    y2 = y(sxi:exi);
+    x2 = x(sxi:exi);
+    [y_max, iy] = max(y2);
+    target_x = x2(iy);
     iy = iy+sxi-1;
   else
-    [y_max, iy] = max(xy(:,2));
-    target_x = xy(iy,1);
-    max_data = [target_x, y_max];
+    [y_max, iy] = max(y);
+    target_x = x(iy);
   endif
-  
+  max_data = [target_x, y_max];
+
   switch (nargout)
     case (0)
       varargout = {max_data};
