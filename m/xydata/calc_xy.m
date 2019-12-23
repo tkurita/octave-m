89470e64136e515ed946e5840cd9f575cf42ece6
Tetsuro Kurita <tkurita@mac.com>
2019-12-23 14:22:43 +0900

    support a struct as a xy parameter

--- m/xydata/calc_xy.m
+++ m/xydata/calc_xy.m
@@ -3,14 +3,6 @@
 ##
 ## @end deftypefn
 
-##== History
-## 2013-11-11
-## * support element by element operation.
-## 2009-06-09
-## * If enough arguments are not given, print_usage().
-## 2009-05-20
-## * First implementaion. Will replace factoryX/Y shift_xy
-
 ##== To Do
 ## * accept inline function or function handle as an argument.
 
@@ -22,23 +14,44 @@ function xy = calc_xy(xy, varargin)
   [px, py, mx, my, ebmx, ebmy] = get_properties(varargin, ...
                     {"x+", "y+", "x*", "y*", "x.*", "y.*"},...
                      {NaN, NaN, NaN, NaN, NaN, NaN});
-  if (!isnan(px))
-    xy(:,1) += px;
-  endif
-  if (!isnan(py))
-    xy(:,2) += py;
-  endif
-  if (!isnan(mx))
-    xy(:,1) *= mx;
-  endif
-  if (!isnan(my))
-    xy(:,2) *= my;
-  endif
-  if ! isnan(ebmx)
-    xy(:,1) .*= ebmx;
-  endif
-  if ! isnan(ebmy)
-    xy(:,2) .*= ebmy;
+  if isstruct(xy)
+    if (!isnan(px))
+      xy.x += px;
+    endif
+    if (!isnan(py))
+      xy.y += py;
+    endif
+    if (!isnan(mx))
+      xy.x *= mx;
+    endif
+    if (!isnan(my))
+      xy.y *= my;
+    endif
+    if ! isnan(ebmx)
+      xy.x .*= ebmx;
+    endif
+    if ! isnan(ebmy)
+      xy.y .*= ebmy;
+    endif
+  else
+    if (!isnan(px))
+      xy(:,1) += px;
+    endif
+    if (!isnan(py))
+      xy(:,2) += py;
+    endif
+    if (!isnan(mx))
+      xy(:,1) *= mx;
+    endif
+    if (!isnan(my))
+      xy(:,2) *= my;
+    endif
+    if ! isnan(ebmx)
+      xy(:,1) .*= ebmx;
+    endif
+    if ! isnan(ebmy)
+      xy(:,2) .*= ebmy;
+    endif
   endif
 endfunction
 
