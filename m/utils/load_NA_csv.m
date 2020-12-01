b7268168df8f7a012b253ab23c6718cf9dc9fda2
Kurita Tetsuro <tkurita@mac.com>
2020-12-01 21:13:17 +0900

    added load_NA_csv

---
+++ m/utils/load_NA_csv.m
@@ -0,0 +1,31 @@
+## -- retval = load_NA_csv(filepath)
+##    load csv file of KEYSIGHT FieldFox
+##
+##  * Inputs *
+##    arg1 : 
+##
+##  * Outputs *
+##    [Hz, dB] or [Hz, degree]
+##    
+##  * Exmaple *
+##
+##  See also: 
+
+function data = load_NA_csv(filepath)
+  fid = open_zip_file(filepath)
+  while(1)
+    l = fgetl(fid);
+    spos = strfind(l, "BEGIN");
+    if (length(spos) && (spos(1) == 1))
+      break;
+    endif
+  endwhile
+  
+  data = csvread(fid);
+  fclose(fid);
+  data(end,:) = [];
+endfunction
+
+
+%!test
+%! func_name(x)
