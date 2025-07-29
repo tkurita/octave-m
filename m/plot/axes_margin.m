0fea9fb69edc0cd70918cb3d5aa01b0a3a703767
Kurita Tetsuro <tkurita@mac.com>
2025-07-29 14:43:18 +0900

    feat: implemanted "left", "top", "bottom" options

--- m/plot/axes_margin.m
+++ m/plot/axes_margin.m
@@ -1,12 +1,11 @@
 ## -- axes_margin(hax, "right", rightmargin)
 ##    axes_margin("right", rightmargin)
+##    axes_margin("left", rightmargin, "top", topmargin, "bottom", bottommargin)
 ##
 ## Set margins to axes.
 ##
 ## * Inputs *
 ##   hax : array of axes axes handles. If ommited, gca is used.
-## 
-## "left", "top", "bottom" options are not implemented.
 
 function axes_margin(varargin)
   if ! ischar(varargin{1})
@@ -16,11 +15,35 @@ function axes_margin(varargin)
     hax = gca();
   endif
 
-  opts = get_properties(varargin, {"right", NA});
+  opts = get_properties(varargin, {"right", NA; 
+                                   "left", NA;
+                                   "bottom", NA;
+                                   "top", NA});
   for ax = hax(:)'
-    pos = get(ax, "position");
+    pos = get(ax, "position")
+    [lm, bm, w, h] = div_elem(pos);
+    rm = 1 - (w + lm);
+    tm = 1 - (h + bm);
+    needset = false;
+    if ! isna(opts.left)
+      lm = opts.left;
+      needset = true;
+    endif
+    if ! isna(opts.bottom)
+      bm = opts.bottom;
+      needset = true;
+    endif    
     if ! isna(opts.right)
-      pos(3) = 1 - opts.right;
+      rm = opts.right;
+      neetset = true;
+    endif
+    if ! isna(opts.top)
+      tm = opts.top;
+      neetset = true;
+    endif
+    if needset
+      pos = [lm, bm, 1 - (lm + rm), 1 - (bm + tm)]
+      disp(pos)
       set(ax, "position", pos);
     endif
   endfor
