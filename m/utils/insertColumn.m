ef3c92f5a938d90d71854d3fbafb45d47ff61969
Kurita Tetsuro <tkurita@mac.com>
2026-06-15 17:15:41 +0900

    deprecated. use insert_column

--- m/utils/insertColumn.m
+++ m/utils/insertColumn.m
@@ -1,5 +1,6 @@
 ## Usage : result = insertColumn(mat, insertMat, colIndex)
 function result = insertColumn(mat, insertMat, colIndex)
+  warning("insertColumn is deprecated. Use insert_column");
   pre_mat = mat(:,1:colIndex-1);
   post_mat = mat(:,colIndex:end);
   result = [pre_mat, insertMat, post_mat];
